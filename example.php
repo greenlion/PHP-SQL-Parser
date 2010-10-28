f8e0fb08fb7f407bc75f044fd104880afc9815bd
Justin Swanhart <greenlion@gmail.com>
2010-10-28 07:00:26 +0000

    Fix a problem where subqueries in the from clause did not properly populate base_expr
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@12 44dcfd11-89b7-3d55-2063-5c365055f77f

--- example.php
+++ example.php
@@ -1,20 +1,6 @@
 <?php
 
 require_once('php-sql-parser.php');
-$sql = '
-select DISTINCT 1+2   c1, 1+ 2 as
-`c2`, sum(c2),"Status" = CASE
-        WHEN quantity > 0 THEN "in stock"
-        ELSE "out of stock"
-        END
-, t4.c1, (select c1+c2 from t1 table limit 1) as subquery into @a1, @a2, @a3 from `table` the_t1 left outer join t2 using(c1,c2) join
-(select a, b, length(concat(a,b,c)) from ( select 1 a,2 b,3 c from some_Table ) table ) subquery_in_from join t3 as tX on tX.c1 = the_t1.c1 natural join t4 t4_x using(cX)  where c1 = 1 and c2 in (1,2,3, "apple") and exists ( select 1 from some_other_table another_table where x > 1) and ("zebra" = "orange" or 1 = 1) group by 1, 2 having sum(c2) > 1 ORDER BY 2, c1 DESC LIMIT 0, 10 into outfile "/xyz" FOR UPDATE LOCK IN SHARE MODE
-UNION ALL
-SELECT NULL,NULL,NULL,NULL,NULL FROM DUAL LIMIT 1';
-$parser = new PHPSQLParser($sql);
-print_r($parser->parsed);
-exit;
-
 
 /*You can use the constuctor for parsing.  The parsed statement is stored at the ->parsed property.*/
 $sql = 'REPLACE INTO table (a,b,c) VALUES (1,2,3)';
@@ -49,8 +35,6 @@ echo $sql . "\n";
 $parser = new PHPSQLParser($sql);
 print_r($parser->parsed);
 
-/*
-
 $sql = "(select `c2`, `c```, \"quoted \'string\' \\\" with `embedded`\\\"\\\" quotes\" as `an``alias` from table table)
 UNION ALL (select `c2`, `c```, \"quoted \'string\' \\\" with `embedded`\\\"\\\" quotes\" as `an``alias` from table table)";
 $parser = new PHPSQLParser($sql);
@@ -65,9 +49,6 @@ $sql = "select `c2`, `c```, \"quoted \'string\' \\\" with `embedded`\\\"\\\" quo
 echo $sql . "\n";
 $parser = new PHPSQLParser($sql);
 print_r($parser->parsed);
-
-
-
 
 $sql = "alter table xyz add key my_key(a,b,c), drop primay key";
 echo $sql . "\n";
@@ -161,4 +142,4 @@ echo $sql . "\n";
 $parser = new PHPSQLParser($sql);
 print_r($parser->parsed);
 
-*/
+
