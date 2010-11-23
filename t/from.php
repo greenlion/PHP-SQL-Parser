c2a578216b2fa47aa703f3ce4b3d5e956f114964
Justin Swanhart <greenlion@gmail.com>
2010-11-23 07:21:17 +0000

    Fix problems with FROM clauses
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@25 44dcfd11-89b7-3d55-2063-5c365055f77f

--- t/from.php
+++ t/from.php
@@ -18,7 +18,7 @@ $sql = 'select DISTINCT 1+2   c1, 1+ 2 as
         WHEN quantity > 0 THEN \'in stock\'
         ELSE \'out of stock\'
         END case_statement
-, t4.c1, (select c1+c2 from t1 inner_t1 limit 1) as subquery into @a1, @a2, @a3 from t1 the_t1 left outer join t2 using(c1,c2) join t3 as tX on tX.c1 = the_t1.c1 natural join t4 t4_x using(cX)  where c1 = 1 and c2 in (1,2,3, "apple") and exists ( select 1 from some_other_table another_table where x > 1) and ("zebra" = "orange" or 1 = 1) group by 1, 2 having sum(c2) > 1 ORDER BY 2, c1 DESC LIMIT 0, 10 into outfile "/xyz" FOR UPDATE LOCK IN SHARE MODE';
+, t4.c1, (select c1+c2 from t1 inner_t1 limit 1) as subquery into @a1, @a2, @a3 from t1 the_t1 left outer join t2 using(c1,c2) join t3 as tX ON tX.c1 = the_t1.c1 join t4 t4_x using(x) where c1 = 1 and c2 in (1,2,3, "apple") and exists ( select 1 from some_other_table another_table where x > 1) and ("zebra" = "orange" or 1 = 1) group by 1, 2 having sum(c2) > 1 ORDER BY 2, c1 DESC LIMIT 0, 10 into outfile "/xyz" FOR UPDATE LOCK IN SHARE MODE';
 
 $parser = new PHPSQLParser($sql);
 $p=$parser->parsed;
@@ -33,7 +33,6 @@ ok($p['SELECT'][4]['alias'] == '`case_statement`');
 ok($p['SELECT'][5]['alias'] == '`t4.c1`');
 ok($p['SELECT'][6]['alias'] == '`subquery`');
 
-exit;
-
+print_r($p['FROM']);
 
 
