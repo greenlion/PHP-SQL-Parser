2e929088ad0f1b72a8d11aac9f40432210884218
Justin Swanhart <greenlion@gmail.com>
2011-04-22 17:49:29 +0000

    Fix issue #7 - problem with > < operations
    git-svn-id: http://php-sql-parser.googlecode.com/svn/trunk@37 44dcfd11-89b7-3d55-2063-5c365055f77f

--- t/gtltop.php
+++ t/gtltop.php
@@ -4,13 +4,14 @@ require_once('../test-more.php');
 $parser = new PHPSQLParser();
 $sql = 'SELECT c1
           from some_table an_alias
-	where d<>0  or d <> 0 or d<>"test1" or d <> "test2";';
+	where d>0 and d>1 and d>-1 and d<2 and d<>0  or d <> 0 or d<>"test1" or d <> "test2";';
 $parser->parse($sql);
 $p = $parser->parsed;
-ok($parser->parsed['WHERE'][1]['base_expr'] == '<>' &&
-$parser->parsed['WHERE'][5]['base_expr'] == '<>' &&
-$parser->parsed['WHERE'][9]['base_expr'] == '<>' &&
-$parser->parsed['WHERE'][13]['base_expr'] == '<>'
-);
 
+$result=serialize($p);
+#$fh = fopen('../r/gtltop.serialized', 'w');
+#fputs($fh, $result);
+#fclose($fh);
+
+ok($result == file_get_contents('../r/gtltop.serialized'));
 
