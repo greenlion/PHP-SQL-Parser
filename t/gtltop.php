abfb44b2322ca8dfb7c710760714891a799afece
Justin Swanhart <greenlion@gmail.com>
2011-10-01 20:44:54 +0000

    >= and <= were not treated correctly
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@45 44dcfd11-89b7-3d55-2063-5c365055f77f

--- t/gtltop.php
+++ t/gtltop.php
@@ -4,9 +4,10 @@ require_once('../test-more.php');
 $parser = new PHPSQLParser();
 $sql = 'SELECT c1
           from some_table an_alias
-	where d>0 and d>1 and d>-1 and d<2 and d<>0  or d <> 0 or d<>"test1" or d <> "test2";';
+	where d>=0 and d>0 and d>1 and d>-1 and d<2 and d<>0  or d <> 0 or d<>"test1" or d <> "test2";';
 $parser->parse($sql);
 $p = $parser->parsed;
+print_r($p);
 $result=serialize($p);
 #$fh = fopen('../r/gtltop.serialized', 'w');
 #fputs($fh, $result);
