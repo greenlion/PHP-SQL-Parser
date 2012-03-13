6cfc24c9306d89aa9d11ad41b87449862d33e733
Andre Rothe <phosco@gmx.de>
2012-03-13 10:09:52 +0000

    The parser has been enhanced to solve issue 15. It is a regex problem. The tokenizer splits now on parenthesis too, so we balance the parameter lists of functions after the tokenizer process. It seems that we have already balanced backticks. Both methods call have been removed. Some tests are invalid.
    git-svn-id: http://php-sql-parser.googlecode.com/svn/trunk@118 44dcfd11-89b7-3d55-2063-5c365055f77f

--- t/issue15.php
+++ t/issue15.php
@@ -3,8 +3,6 @@
 require_once(dirname(__FILE__) . "/../php-sql-parser.php");
 require_once(dirname(__FILE__) . "/../test-more.php");
 
-# not solved
-
 $parser = new PHPSQLParser();
 
 $sql = "select usr_id, usr_login, case id_tipousuario when 1 then 'Usuario CVE' when 2 then concat('Usuario Vendedor -', codigovendedor, '-') when 3 then concat('Usuario Vendedor Meson (', codigovendedor, ')') end tipousuario, CONCAT( usr_nombres, ' ', usr_apellidos ) as nom_com, cod_local from usuarios where usr_estado <> 2 order by 3, 1, 4";
