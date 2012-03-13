9eed0f0614d4657070788d2ff0007f7fbf2cecae
Andre Rothe <phosco@gmx.de>
2012-03-13 08:25:33 +0000

    Some checks like
    if (!$token) 
    
    have been replaced with
    
    if ($token === "")
    
    because a token can contain "0", which will be recognized as false.
    Some Tests have been written which base on issues from the website.
    
    git-svn-id: http://php-sql-parser.googlecode.com/svn/trunk@52 44dcfd11-89b7-3d55-2063-5c365055f77f

---
+++ t/issue21.php
@@ -0,0 +1,18 @@
+<?php
+require_once(dirname(__FILE__) . "/../php-sql-parser.php");
+require_once(dirname(__FILE__) . "/../test-more.php");
+
+$parser = new PHPSQLParser();
+
+$sql = 'SELECT  SUM( 10 ) as test FROM account';
+$p = $parser->parse($sql);
+
+$expected = getExpectedValue('issue21.serialized');
+eq_array($p, $expected, 'only space characters within SQL statement');
+
+
+$sql = 'SELECT 	SUM( 10 ) as test FROM account';
+$p = $parser->parse($sql);
+
+$expected = getExpectedValue('issue21.serialized'); // should be the same as above
+eq_array($p, $expected, 'tab character within SQL statement');
