2ff78e3c92ca08b491ecded962b39345cc6289f6
Andre Rothe <phosco@gmx.de>
2012-03-13 08:29:03 +0000

    The caller code has been moved into its own function, so we can use the file also for successful tests.
    git-svn-id: http://php-sql-parser.googlecode.com/svn/trunk@55 44dcfd11-89b7-3d55-2063-5c365055f77f

--- test-more.php
+++ test-more.php
@@ -1,42 +1,42 @@
 <?php
 /*******************************************************************\
-*                        PROJECT INFORMATION                        *
-*                                                                   *
-*  Project:  Apache-Test                                            *
-*  URL:      http://perl.apache.org/Apache-Test/                    *
-*  Notice:   Copyright (c) 2006 The Apache Software Foundation      *
-*                                                                   *
-*********************************************************************
-*                        LICENSE INFORMATION                        *
-*                                                                   *
-*  Licensed under the Apache License, Version 2.0 (the "License");  *
-*  you may not use this file except in compliance with the          *
-*  License. You may obtain a copy of the License at:                *
-*                                                                   *
-*  http://www.apache.org/licenses/LICENSE-2.0                       *
-*                                                                   *
-*  Unless required by applicable law or agreed to in writing,       *
-*  software distributed under the License is distributed on an "AS  *
-*  IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either  *
-*  express or implied. See the License for the specific language    *
-*  governing permissions and limitations under the License.         *
-*                                                                   *
-*********************************************************************
-*                        MODULE INFORMATION                         *
-*                                                                   *
-*  This is a PHP implementation of Test::More:                      *
-*                                                                   *
-*  http://search.cpan.org/dist/Test-Simple/lib/Test/More.pm         *
-*                                                                   *
-*********************************************************************
-*                              CREDITS                              *
-*                                                                   *
-*  Originally inspired by work from Andy Lester. Written and        *
-*  maintained by Chris Shiflett. For contact information, see:      *
-*                                                                   *
-*  http://shiflett.org/contact                                      *
-*                                                                   *
-\*******************************************************************/
+ *                        PROJECT INFORMATION                        *
+ *                                                                   *
+ *  Project:  Apache-Test                                            *
+ *  URL:      http://perl.apache.org/Apache-Test/                    *
+ *  Notice:   Copyright (c) 2006 The Apache Software Foundation      *
+ *                                                                   *
+ *********************************************************************
+ *                        LICENSE INFORMATION                        *
+ *                                                                   *
+ *  Licensed under the Apache License, Version 2.0 (the "License");  *
+ *  you may not use this file except in compliance with the          *
+ *  License. You may obtain a copy of the License at:                *
+ *                                                                   *
+ *  http://www.apache.org/licenses/LICENSE-2.0                       *
+ *                                                                   *
+ *  Unless required by applicable law or agreed to in writing,       *
+ *  software distributed under the License is distributed on an "AS  *
+ *  IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either  *
+ *  express or implied. See the License for the specific language    *
+ *  governing permissions and limitations under the License.         *
+ *                                                                   *
+ *********************************************************************
+ *                        MODULE INFORMATION                         *
+ *                                                                   *
+ *  This is a PHP implementation of Test::More:                      *
+ *                                                                   *
+ *  http://search.cpan.org/dist/Test-Simple/lib/Test/More.pm         *
+ *                                                                   *
+ *********************************************************************
+ *                              CREDITS                              *
+ *                                                                   *
+ *  Originally inspired by work from Andy Lester. Written and        *
+ *  maintained by Chris Shiflett. For contact information, see:      *
+ *                                                                   *
+ *  http://shiflett.org/contact                                      *
+ *                                                                   *
+ \*******************************************************************/
 
 header('Content-Type: text/plain');
 register_shutdown_function('_test_end');
@@ -48,257 +48,264 @@ $_test_num = 0;
 
 function plan($plan)
 {
-    /*
-    plan('no_plan');
-    plan('skip_all');
-    plan(array('skip_all' => 'My reason is...'));
-    plan(23);
-    */
+	/*
+	 plan('no_plan');
+	 plan('skip_all');
+	 plan(array('skip_all' => 'My reason is...'));
+	 plan(23);
+	 */
 
-    global $_no_plan;
-    global $_skip_all;
+	global $_no_plan;
+	global $_skip_all;
 
-    switch ($plan) {
-        case 'no_plan':
-            $_no_plan = TRUE;
-            break;
+	switch ($plan) {
+		case 'no_plan':
+			$_no_plan = TRUE;
+			break;
 
-        case 'skip_all':
-            echo "1..0\n";
-            break;
+		case 'skip_all':
+			echo "1..0\n";
+			break;
 
-        default:
-            if (is_array($plan)) {
-                echo "1..0 # Skip {$plan['skip_all']}\n";
-                exit;
-            }
+		default:
+			if (is_array($plan)) {
+				echo "1..0 # Skip {$plan['skip_all']}\n";
+				exit;
+			}
 
-            echo "1..$plan\n";
-            break;
-    }
+			echo "1..$plan\n";
+			break;
+	}
+}
+
+function get_origin() {
+	$res = array();
+	$caller = debug_backtrace();
+
+	$thisFile = __FILE__;
+
+	$i = 0;
+	while (strstr($caller[$i]['file'], $thisFile)) {
+		$i++;
+	}
+
+	$res['file'] = $caller[$i]['file'];
+	$res['line'] = $caller[$i]['line'];
+	
+	if (isset($_SERVER['SERVER_ROOT'])){
+		$res['file'] = str_replace($_SERVER['SERVER_ROOT'], 't', $res['file']);
+	}
+	return $res;
 }
 
 function ok($pass, $test_name = '')
 {
-    global $_test_num;
-    global $_num_failures;
-    global $_num_skips;
+	global $_test_num;
+	global $_num_failures;
+	global $_num_skips;
 
-    $_test_num++; 
+	$_test_num++;
 
-    if ($_num_skips) {
-        $_num_skips--;
-        return TRUE;
-    }
+	if ($_num_skips) {
+		$_num_skips--;
+		return TRUE;
+	}
 
-    if (!empty($test_name) && $test_name[0] != '#') {
-        $test_name = "- $test_name";
-    }
+	if (!empty($test_name) && $test_name[0] != '#') {
+		$test_name = "- $test_name";
+	}
 
-    if ($pass) {
-        echo "ok $_test_num $test_name\n";
-    } else {
-        echo "not ok $_test_num $test_name\n";
+	$origin = get_origin();
+	$file = $origin['file'];
+	$line = $origin['line'];
 
-        $_num_failures++;
-        $caller = debug_backtrace();
+	if ($pass) {
+		echo "ok $_test_num $test_name\n";
+	} else {
+		diag("    Failed test ($file at line $line)");
+		echo "not ok $_test_num $test_name\n";
+		$_num_failures++;
+	}
 
-        $thisFile = __FILE__;
-
-        $i = 0;
-        while (strstr($caller[$i]['file'], $thisFile)) {
-                $i++;
-        }
-
-        $file = $caller[$i]['file'];
-        $line = $caller[$i]['line'];
-
-        if (isset($_SERVER['SERVER_ROOT'])){
-            $file = str_replace($_SERVER['SERVER_ROOT'], 't', $file);
-        }
-
-        diag("    Failed test ($file at line $line)");
-    }
-
-    return $pass;
+	return $pass;
 }
 
 function is($this, $that, $test_name = '')
 {
-    $pass = ($this == $that);
+	$pass = ($this == $that);
 
-    ok($pass, $test_name);
+	ok($pass, $test_name);
 
-    if (!$pass) {
-        diag("         got: '$this'");
-        diag("    expected: '$that'");
-    }
+	if (!$pass) {
+		diag("         got: '$this'");
+		diag("    expected: '$that'");
+	}
 
-    return $pass;
+	return $pass;
 }
 
 function isnt($this, $that, $test_name = '')
 {
-    $pass = ($this != $that);
+	$pass = ($this != $that);
 
-    ok($pass, $test_name);
+	ok($pass, $test_name);
 
-    if (!$pass) {
-        diag("    '$this'");
-        diag('        !=');
-        diag("    '$that'");
-    }
+	if (!$pass) {
+		diag("    '$this'");
+		diag('        !=');
+		diag("    '$that'");
+	}
 
-    return $pass;
+	return $pass;
 }
 
 function like($string, $pattern, $test_name = '')
 {
-    $pass = preg_match($pattern, $string);
+	$pass = preg_match($pattern, $string);
 
-    ok($pass, $test_name);
+	ok($pass, $test_name);
 
-    if (!$pass) {
-        diag("                  '$string'");
-        diag("    doesn't match '$pattern'");
-    }
+	if (!$pass) {
+		diag("                  '$string'");
+		diag("    doesn't match '$pattern'");
+	}
 
-    return $pass;
+	return $pass;
 }
 
 function unlike($string, $pattern, $test_name = '')
 {
-    $pass = !preg_match($pattern, $string);
+	$pass = !preg_match($pattern, $string);
 
-    ok($pass, $test_name);
+	ok($pass, $test_name);
 
-    if (!$pass) {
-        diag("                  '$string'");
-        diag("          matches '$pattern'");
-    }
+	if (!$pass) {
+		diag("                  '$string'");
+		diag("          matches '$pattern'");
+	}
 
-    return $pass;
+	return $pass;
 }
 
 function cmp_ok($this, $operator, $that, $test_name = '')
 {
-    eval("\$pass = (\$this $operator \$that);");
+	eval("\$pass = (\$this $operator \$that);");
 
-    ob_start();
-    var_dump($this);
-    $_this = trim(ob_get_clean());
+	ob_start();
+	var_dump($this);
+	$_this = trim(ob_get_clean());
 
-    ob_start();
-    var_dump($that);
-    $_that = trim(ob_get_clean());
+	ob_start();
+	var_dump($that);
+	$_that = trim(ob_get_clean());
 
-    ok($pass, $test_name);
+	ok($pass, $test_name);
 
-    if (!$pass) {
-        diag("         got: $_this");
-        diag("    expected: $_that");
-    }
+	if (!$pass) {
+		diag("         got: $_this");
+		diag("    expected: $_that");
+	}
 
-    return $pass;
+	return $pass;
 }
 
 function can_ok($object, $methods)
 {
-    $pass = TRUE;
-    $errors = array();
+	$pass = TRUE;
+	$errors = array();
 
-    foreach ($methods as $method) {
-        if (!method_exists($object, $method)) {
-            $pass = FALSE;
-            $errors[] = "    method_exists(\$object, $method) failed";
-        }
-    }
+	foreach ($methods as $method) {
+		if (!method_exists($object, $method)) {
+			$pass = FALSE;
+			$errors[] = "    method_exists(\$object, $method) failed";
+		}
+	}
 
-    if ($pass) {
-        ok(TRUE, "method_exists(\$object, ...)");
-    } else {
-        ok(FALSE, "method_exists(\$object, ...)");
-        diag($errors);
-    }
+	if ($pass) {
+		ok(TRUE, "method_exists(\$object, ...)");
+	} else {
+		ok(FALSE, "method_exists(\$object, ...)");
+		diag($errors);
+	}
 
-    return $pass;
+	return $pass;
 }
 
 function isa_ok($object, $expected_class, $object_name = 'The object')
 {
-    $got_class = get_class($object);
+	$got_class = get_class($object);
 
-    if (version_compare(phpversion(), '5', '>=')) {
-        $pass = ($got_class == $expected_class);
-    } else {
-        $pass = ($got_class == strtolower($expected_class));
-    }
+	if (version_compare(phpversion(), '5', '>=')) {
+		$pass = ($got_class == $expected_class);
+	} else {
+		$pass = ($got_class == strtolower($expected_class));
+	}
 
-    if ($pass) {
-        ok(TRUE, "$object_name isa $expected_class");
-    } else {
-        ok(FALSE, "$object_name isn't a '$expected_class' it's a '$got_class'");
-    }
+	if ($pass) {
+		ok(TRUE, "$object_name isa $expected_class");
+	} else {
+		ok(FALSE, "$object_name isn't a '$expected_class' it's a '$got_class'");
+	}
 
-    return $pass;
+	return $pass;
 }
 
 function pass($test_name = '')
 {
-    return ok(TRUE, $test_name);
+	return ok(TRUE, $test_name);
 }
 
 function fail($test_name = '')
 {
-    return ok(FALSE, $test_name);
+	return ok(FALSE, $test_name);
 }
 
 function diag($message)
 {
-    if (is_array($message)) {
-        foreach($message as $current) {
-            echo "# $current\n";
-        }
-    } else {
-        echo "# $message\n";
-    }
+	if (is_array($message)) {
+		foreach($message as $current) {
+			echo "# $current\n";
+		}
+	} else {
+		echo "# $message\n";
+	}
 }
 
 function include_ok($module)
 {
-    $pass = ((include $module) == 1);
-    return ok($pass);
+	$pass = ((include $module) == 1);
+	return ok($pass);
 }
 
 function require_ok($module)
 {
-    $pass = ((require $module) == 1);
-    return ok($pass);
-} 
+	$pass = ((require $module) == 1);
+	return ok($pass);
+}
 
 function skip($message, $num)
 {
-    global $_num_skips;
+	global $_num_skips;
 
-    if ($num < 0) {
-        $num = 0;
-    }
+	if ($num < 0) {
+		$num = 0;
+	}
 
-    for ($i = 0; $i < $num; $i++) {
-        pass("# SKIP $message");
-    }
+	for ($i = 0; $i < $num; $i++) {
+		pass("# SKIP $message");
+	}
 
-    $_num_skips = $num;
+	$_num_skips = $num;
 }
 
 function eq_array($arrA, $arrB, $testName)
 {
-    $result = serialize($arrA) === serialize($arrB);
-    if ($result) {
-        pass($testName);
-    } else {
-        fail($testName);
-    }
+	$result = serialize($arrA) === serialize($arrB);
+	if ($result) {
+		pass($testName);
+	} else {
+		fail($testName);
+	}
 }
 
 /*
@@ -333,17 +340,17 @@ function eq_set()
 
 function _test_end()
 {
-    global $_no_plan;
-    global $_num_failures;
-    global $_test_num;
+	global $_no_plan;
+	global $_num_failures;
+	global $_test_num;
 
-    if ($_no_plan) {
-        echo "1..$_test_num\n";
-    }
+	if ($_no_plan) {
+		echo "1..$_test_num\n";
+	}
 
-    if ($_num_failures) {
-        diag("Looks like you failed $_num_failures tests of $_test_num.");
-    }
+	if ($_num_failures) {
+		diag("Looks like you failed $_num_failures tests of $_test_num.");
+	}
 }
 
 /**
