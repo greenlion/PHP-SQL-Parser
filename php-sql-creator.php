6cb876c3672ea149afd87026dbd7736f9763c636
Andre Rothe <phosco@gmx.de>
2012-03-13 09:24:26 +0000

    The file tests.php has been renamed to tests-parser.php, because we have now tests for the creator too. A new version of the creator has base functionality to create simple SELECT queries.
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@89 44dcfd11-89b7-3d55-2063-5c365055f77f

--- php-sql-creator.php
+++ php-sql-creator.php
@@ -4,47 +4,154 @@ error_reporting(E_ALL);
 if (!defined('HAVE_PHP_SQL_CREATOR')) {
 
     class PHPSQLCreator {
-        
+
         public function __construct($parsed = false) {
             if ($parsed) {
                 $this->create($parsed);
             }
         }
-        
+
         public function create($parsed) {
             $k = key($parsed);
             switch ($k) {
-                
-                case "UNION":
-                case "UNION ALL":
-                    die("UNIONS");
-                    break;
-                case "SELECT":
-                    $this->created = $this->processSelectStatement($parsed);
-                    break;
-                case "INSERT":
-                    $this->created = $this->processInsertStatement($parsed);
-                    break;
-                case "DELETE":
-                    $this->created = $this->processDeleteStatement($parsed);
-                    break;
-                case "UPDATE":
-                    $this->created = $this->processUpdateStatement($parsed);
-                    break;
-                default:
-                    die("unknown key " . $k);
+
+            case "UNION":
+            case "UNION ALL":
+                die("UNIONS");
+                break;
+            case "SELECT":
+                $this->created = $this->processSelectStatement($parsed);
+                break;
+            case "INSERT":
+                $this->created = $this->processInsertStatement($parsed);
+                break;
+            case "DELETE":
+                $this->created = $this->processDeleteStatement($parsed);
+                break;
+            case "UPDATE":
+                $this->created = $this->processUpdateStatement($parsed);
+                break;
+            default:
+                die("unknown key " . $k);
                 break;
             }
         }
 
         private function processSelectStatement($parsed) {
-            
+            $sql = $this->processSELECT($parsed['SELECT']) . " " . $this->processFROM($parsed['FROM']);
+            if (isset($parsed['WHERE'])) {
+                $sql .= " " . $this->processWHERE($parsed['WHERE']);
+            }
+            if (isset($parsed['ORDER'])) {
+                $sql .= " " . $this->processORDER($parsed['ORDER']);
+            }
+            if (isset($parsed['GROUP'])) {
+                $sql .= " " . $this->processGROUP($parsed['GROUP']);
+            }
+            return $sql;
+        }
+
+        private function processSELECT($parsed) {
+            $values = "";
+            foreach ($parsed as $k => $v) {
+                if ($v['expr_type'] === 'colref') {
+                    $values .= $v['base_expr'] . ",";
+                } else {
+                    die("unknown expr_type in SELECT[" . $k . "] " . $v['expr_type']);
+                }
+            }
+            $values = substr($values, 0, strlen($values) - 1);
+            return "SELECT " . $values;
+        }
+
+        private function processRefClause($parsed) {
+            if ($parsed === false) {
+                return "";
+            }
+
+            $sql = "";
+            foreach ($parsed as $k => $v) {
+                $sql .= $v['base_expr'] . " ";
+            }
+            return "(" . $sql . ")";
+        }
+
+        private function processAlias($parsed) {
+            if ($parsed === false) {
+                return "";
+            }
+
+            $sql = "";
+            if ($parsed['as']) {
+                $sql .= " as";
+            }
+            $sql .= " " . $parsed['name'];
+            return $sql;
+        }
+
+        private function processJoin($parsed) {
+            if ($parsed === 'CROSS') {
+                return ",";
+            }
+            if ($parsed === 'JOIN') {
+                return "INNER JOIN";
+            }
+            // TODO: add more
         }
         
+        private function processTable($parsed, $key) {
+            if ($parsed['expr_type'] !== 'table') {
+                return "";
+            }
+
+            $sql = $parsed['table'];
+            $sql .= $this->processAlias($parsed['alias']);
+
+            if ($key !== 0) {
+                $sql = $this->processJoin($parsed['join_type']) . " " . $sql;
+                if ($parsed['ref_type'] === 'ON') {
+                    $sql .= " ON ";
+                }
+                $sql .= $this->processRefClause($parsed['ref_clause']);
+            }
+            return $sql;
+        }
+
+        private function processFROM($parsed) {
+            $values = "";
+            foreach ($parsed as $k => $v) {
+                $values .= $this->processTable($v, $k) . " ";
+
+                if ($v['expr_type'] !== 'table') {
+                    die("unknown expr_type in FROM[" . $k . "] " . $v['expr_type']);
+                }
+            }
+            return "FROM " . $values;
+        }
+
+        private function processORDER($parsed) {
+            $values = "";
+            foreach ($parsed as $k => $v) {
+                if ($v['type'] === 'expression') {
+                    $values .= $v['base_expr'] . " " . $v['direction'] . ",";
+                } else {
+                    die("unknown type in ORDER[" . $k . "] " . $v['type']);
+                }
+            }
+            $values = substr($values, 0, strlen($values) - 1);
+            return "ORDER BY " . $values;
+        }
+
+        private function processGROUP($parsed) {
+            $sql = "GROUP BY ";
+
+            return $sql;
+        }
+
         private function processInsertStatement($parsed) {
             return $this->processINSERT($parsed['INSERT']) . " " . $this->processVALUES($parsed['VALUES']);
             # TODO: subquery?
-        } 
+        }
 
         private function processVALUES($parsed) {
             $values = "";
@@ -55,10 +162,10 @@ if (!defined('HAVE_PHP_SQL_CREATOR')) {
             $sql = "VALUES (" . $values . ")";
             return $sql;
         }
-        
+
         private function processINSERT($parsed) {
             $sql = "INSERT INTO " . $parsed['table'];
-            
+
             $columns = "";
             foreach ($parsed['columns'] as $k => $v) {
                 $columns .= $v['base_expr'] . ",";
@@ -69,23 +176,23 @@ if (!defined('HAVE_PHP_SQL_CREATOR')) {
             $sql .= $columns;
             return $sql;
         }
-        
+
         private function processDeleteStatement($parsed) {
-            
+
         }
-        
+
         private function processUpdateStatement($parsed) {
-           $sql = $this->processUPDATE($parsed['UPDATE']) . " " . $this->processSET($parsed['SET']);
-           if (isset($parsed['WHERE'])) {
-               $sql .=  " " .$this->processWHERE($parsed['WHERE']);
-           }
-           return $sql;
+            $sql = $this->processUPDATE($parsed['UPDATE']) . " " . $this->processSET($parsed['SET']);
+            if (isset($parsed['WHERE'])) {
+                $sql .= " " . $this->processWHERE($parsed['WHERE']);
+            }
+            return $sql;
         }
-        
+
         private function processUPDATE($parsed) {
-            return "UPDATE ".$parsed[0]['table'];
+            return "UPDATE " . $parsed[0]['table'];
         }
-        
+
         private function processSET($parsed) {
             $values = "";
             foreach ($parsed as $k => $v) {
@@ -95,7 +202,7 @@ if (!defined('HAVE_PHP_SQL_CREATOR')) {
             $sql = "SET " . $values;
             return $sql;
         }
-        
+
         private function processWHERE($parsed) {
             $sql = "WHERE ";
             foreach ($parsed as $k => $v) {
@@ -104,8 +211,7 @@ if (!defined('HAVE_PHP_SQL_CREATOR')) {
             # subquery, expressions?
             return $sql;
         }
-        
-        
+
     } // END CLASS
     define('HAVE_PHP_SQL_CREATOR', 1);
 }
