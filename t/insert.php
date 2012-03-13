df298dfc632e275ddd5ba52ba84f004c0309f955
Andre Rothe <phosco@gmx.de>
2012-03-13 09:54:19 +0000

    Add a fix for user-functions within the VALUE part of an INSERT statement. A test case has been added.
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@106 44dcfd11-89b7-3d55-2063-5c365055f77f

--- t/insert.php
+++ t/insert.php
@@ -13,3 +13,9 @@ $sql = " INSERT INTO settings_global VALUES ('DBVersion', '146')";
 $p = $parser->parse($sql, true);
 $expected = getExpectedValue('insert2.serialized');
 eq_array($p, $expected, 'insert some data into table with positions');
+
+$sql = "INSERT INTO surveys ( SID, OWNER_ID, ADMIN, ACTIVE, EXPIRES, STARTDATE, ADMINEMAIL, ANONYMIZED, FAXTO, FORMAT, SAVETIMINGS, TEMPLATE, LANGUAGE, DATESTAMP, USECOOKIE, ALLOWREGISTER, ALLOWSAVE, AUTOREDIRECT, ALLOWPREV, PRINTANSWERS, IPADDR, REFURL, DATECREATED, PUBLICSTATISTICS, PUBLICGRAPHS, LISTPUBLIC, HTMLEMAIL, TOKENANSWERSPERSISTENCE, ASSESSMENTS, USECAPTCHA, BOUNCE_EMAIL, EMAILRESPONSETO, EMAILNOTIFICATIONTO, TOKENLENGTH, SHOWXQUESTIONS, SHOWGROUPINFO, SHOWNOANSWER, SHOWQNUMCODE, SHOWWELCOME, SHOWPROGRESS, ALLOWJUMPS, NAVIGATIONDELAY, NOKEYBOARD, ALLOWEDITAFTERCOMPLETION ) 
+VALUES ( 32225, 1, 'André', 'N', null, null, 'hello@zks.uni-leipzig.de', 'N', '', 'G', 'N', 'default', 'de-informal', 'N', 'N', 'N', 'Y', 'N', 'N', 'N', 'N', 'N', a_function('2012-02-16','YYYY-MM-DD'), 'N', 'N', 'Y', 'Y', 'N', 'N', 'D', 'hello@zks.uni-leipzig.de', '', '', 15, 'Y', 'B', 'Y', 'X', 'Y', 'Y', 'N', 0, 'N', 'N' )";
+$p = $parser->parse($sql);
+$expected = getExpectedValue('insert3.serialized');
+eq_array($p, $expected, 'insert with user-function');
