4d898e2098aa579cb56e0648908d4ba044818f18
Andre Rothe <phosco@gmx.de>
2012-03-13 10:44:35 +0000

    Some tab characters have been included in test 2 of issue 21.
    git-svn-id: http://php-sql-parser.googlecode.com/svn/trunk@132 44dcfd11-89b7-3d55-2063-5c365055f77f

--- t/issue21.php
+++ t/issue21.php
@@ -10,7 +10,7 @@ $expected = getExpectedValue('issue21.serialized');
 eq_array($p, $expected, 'only space characters within SQL statement');
 
 
-$sql = 'SELECT 	SUM( 10 ) as test FROM account';
+$sql = "SELECT\tSUM( 10 ) \tas test FROM account";
 $p = $parser->parse($sql);
 $expected = getExpectedValue('issue21.serialized'); // should be the same as above
 eq_array($p, $expected, 'tab character within SQL statement');
