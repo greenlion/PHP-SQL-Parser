a2f111edefa1db516491a90bfaa79694444d97bd
Andre Rothe <phosco@gmx.de>
2012-03-13 09:58:57 +0000

    A new example for insert statements with user-functions has been added.
    git-svn-id: http://php-sql-parser.googlecode.com/svn/trunk@109 44dcfd11-89b7-3d55-2063-5c365055f77f

--- OracleSQLTranslator.php
+++ OracleSQLTranslator.php
@@ -91,7 +91,7 @@ class OracleSQLTranslator extends PHPSQLCreator {
 
     protected function processSELECT($parsed) {
         global $preventColumnRefs;
-        
+
         $sql = parent::processSELECT($parsed);
         if ($preventColumnRefs) {
             $sql = "SELECT *";
@@ -125,6 +125,7 @@ class OracleSQLTranslator extends PHPSQLCreator {
         }
 
         $parser = new PHPSQLParser($sql, true);
+        print_r($parser->parsed);
         $sql = $this->create($parser->parsed);
 
         echo $sql . "\n";
@@ -138,15 +139,25 @@ class OracleSQLTranslator extends PHPSQLCreator {
                             . substr($sql, $start + strlen($columnInfo));
  */
 
-$parser = new OracleSQLParser(false);
+$parser = new OracleSQLTranslator(false);
+
+$sql = "INSERT INTO surveys ( SID, OWNER_ID, ADMIN, ACTIVE, EXPIRES, STARTDATE, ADMINEMAIL, ANONYMIZED, FAXTO, FORMAT, SAVETIMINGS, TEMPLATE, LANGUAGE, DATESTAMP, USECOOKIE, ALLOWREGISTER, ALLOWSAVE, AUTOREDIRECT, ALLOWPREV, PRINTANSWERS, IPADDR, REFURL, DATECREATED, PUBLICSTATISTICS, PUBLICGRAPHS, LISTPUBLIC, HTMLEMAIL, TOKENANSWERSPERSISTENCE, ASSESSMENTS, USECAPTCHA, BOUNCE_EMAIL, EMAILRESPONSETO, EMAILNOTIFICATIONTO, TOKENLENGTH, SHOWXQUESTIONS, SHOWGROUPINFO, SHOWNOANSWER, SHOWQNUMCODE, SHOWWELCOME, SHOWPROGRESS, ALLOWJUMPS, NAVIGATIONDELAY, NOKEYBOARD, ALLOWEDITAFTERCOMPLETION ) 
+VALUES ( 32225, 1, 'André', 'N', null, null, 'hello@zks.uni-leipzig.de', 'N', '', 'G', 'N', 'default', 'de-informal', 'N', 'N', 'N', 'Y', 'N', 'N', 'N', 'N', 'N', a_function('2012-02-16','YYYY-MM-DD'), 'N', 'N', 'Y', 'Y', 'N', 'N', 'D', 'hello@zks.uni-leipzig.de', '', '', 15, 'Y', 'B', 'Y', 'X', 'Y', 'Y', 'N', 0, 'N', 'N' )";
+$parser->process($sql);
+
+$parser->process(
+        "INSERT INTO users (users_name, password, full_name, parent_id, lang ,email, create_survey,create_user ,delete_user ,superadmin ,configurator ,manage_template , manage_label) VALUES ('admin', to_clob('92e32ca895ca2efd049dcfd79f47b19a6e2dc5f915fbd39e807e6775ae7569c3'), 'Your Name', 0, 'en', 'your-email@example.net', 1,1,1,1,1,1,1)");
+
+$parser->process("INSERT INTO settings_global VALUES ('DBVersion','146')");
 $parser->process(
         "SELECT a.*, c.*, u.users_name FROM SURVEYS as a  INNER JOIN SURVEYS_LANGUAGESETTINGS as c ON ( surveyls_survey_id = a.sid AND surveyls_language = a.language ) AND surveyls_survey_id=a.sid and surveyls_language=a.language  INNER JOIN USERS as u ON (u.uid=a.owner_id)  ORDER BY surveyls_title");
 $parser->process(
         " SELECT *, surveyls_title, surveyls_description, surveyls_welcometext, surveyls_url  FROM SURVEYS AS a INNER JOIN SURVEYS_LANGUAGESETTINGS on (surveyls_survey_id=a.sid and surveyls_language=a.language)  order by active DESC, surveyls_title");
-//$parser->process(" INSERT INTO settings_global VALUES ('DBVersion', '146')");
+
 //$parser->process("CREATE TABLE answers ( qid number(11) default '0' NOT NULL, code varchar2(5) default '' NOT NULL, answer CLOB NOT NULL, assessment_value number(11) default '0' NOT NULL, sortorder number(11) NOT NULL, language varchar2(20) default 'en', scale_id number(3) default '0' NOT NULL, PRIMARY KEY (qid,code,language,scale_id) )");
 //$parser->process("USE DATABASE `sdbprod`");
-//$parser->process("insert into SETTINGS_GLOBAL (stg_value,stg_name) values('','force_ssl')");
+$parser->process("insert into SETTINGS_GLOBAL (stg_value,stg_name) values('','force_ssl')");
+
 //$parser->process("SELECT * FROM SETTINGS_GLOBAL");
 //$parser->process("SELECT stg_value FROM SETTINGS_GLOBAL where stg_name='force_ssl'");
 //$parser->process("update SETTINGS_GLOBAL set stg_value='' where stg_name='force_ssl'");
