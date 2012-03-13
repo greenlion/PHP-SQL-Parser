7ae0a03da72b6081399b2bbc8f1e41d3f6aa1b5f
Andre Rothe <phosco@gmx.de>
2012-03-13 10:06:01 +0000

    The native statements have been removed, because LimeSurvey can handle that in another way. A really big test case has been added (for INSERT statements).
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@115 44dcfd11-89b7-3d55-2063-5c365055f77f

--- OracleSQLTranslator.php
+++ OracleSQLTranslator.php
@@ -105,31 +105,8 @@ class OracleSQLTranslator extends PHPSQLCreator {
         return $sql;
     }
 
-    # prevents parsing of create-oracle.php statements
-    # maybe we run into problems with Limesurvey internal creation statements
-    private function isNativeStatement($sql) {
-        $sql = trim($sql);
-        if (stripos($sql, "CREATE") === 0) {
-            return true;
-        }
-        if (stripos($sql, "ALTER") === 0) {
-            return true;
-        }
-        if (stripos($sql, "COMMENT") === 0) {
-            return true;
-        }
-        if (stripos($sql, "USE") === 0) {
-            return true;
-        }
-        return false;
-    }
-
     public function process($sql) {
-        if ($this->isNativeStatement($sql)) {
-            return $sql;
-        }
-
-        $parser = new PHPSQLParser($sql, true);
+        $parser = new PHPSQLParser($sql);
         print_r($parser->parsed);
         $sql = $this->create($parser->parsed);
 
@@ -146,25 +123,120 @@ class OracleSQLTranslator extends PHPSQLCreator {
 
 $parser = new OracleSQLTranslator(false);
 
-$sql = "SELECT a.uid, a.users_name FROM USERS AS a LEFT JOIN (SELECT uid AS id FROM USER_IN_GROUPS WHERE ugid = 1) AS b ON a.uid = b.id WHERE id IS NULL ORDER BY a.users_name";
+
+$sql = "INSERT INTO surveys_lngsettings ( SURVEYLS_SURVEY_ID, SURVEYLS_LANGUAGE, SURVEYLS_TITLE, SURVEYLS_DESCRIPTION, SURVEYLS_WELCOMETEXT, SURVEYLS_ENDTEXT, SURVEYLS_URL, SURVEYLS_URLDESCRIPTION, SURVEYLS_EMAIL_INVITE_SUBJ, SURVEYLS_EMAIL_INVITE, SURVEYLS_EMAIL_REMIND_SUBJ, SURVEYLS_EMAIL_REMIND, SURVEYLS_EMAIL_REGISTER_SUBJ, SURVEYLS_EMAIL_REGISTER, SURVEYLS_EMAIL_CONFIRM_SUBJ, SURVEYLS_EMAIL_CONFIRM, SURVEYLS_DATEFORMAT, EMAIL_ADMIN_NOTIFICATION_SUBJ, EMAIL_ADMIN_NOTIFICATION, EMAIL_ADMIN_RESPONSES_SUBJ, EMAIL_ADMIN_RESPONSES, SURVEYLS_NUMBERFORMAT ) 
+VALUES ( 53313, 'de-informal', 'Mappensurvey', 'Hier antworten nur Mappen!', 'Hallo Du Mappe', 'Geh heim du Mappe!', '', '', 'Einladung zur einer Umfrage', 'Hallo {FIRSTNAME},
+
+Hiermit möchten wir dich zu einer Umfrage einladen.
+
+Der Titel der Umfrage ist
+''{SURVEYNAME}''
+
+''{SURVEYDESCRIPTION}''
+
+Um an dieser Umfrage teilzunehmen, klicke bitte auf den unten stehenden Link.
+
+Mit freundlichen Grüßen,
+
+{ADMINNAME} ({ADMINEMAIL})a
+
+----------------------------------------------
+Klicke hier um die Umfrage zu starten:
+{SURVEYURL}
+
+Wenn Du an dieser Umfrage nicht teilnehmen und keine weiteren Erinnerungen erhalten möchtest, klicke bitte auf den folgenden Link:
+{OPTOUTURL}', 'Erinnerung an die Teilnahme an einer Umfrage', 'Hallo {FIRSTNAME},
+
+Vor kurzem haben wir Dich zu einer Umfrage eingeladen.
+
+Zu unserem Bedauern haben wir bemerkt, dass Du die Umfrage noch nicht ausgefüllt hast. Wir möchten Dir mitteilen, dass die Umfrage noch aktiv ist und würden uns freuen, wenn Du teilnehmen könntest.
+
+Der Titel der Umfrage ist
+''{SURVEYNAME}''
+
+''{SURVEYDESCRIPTION}''
+
+Um an dieser Umfrage teilzunehmen, klicke bitte auf den unten stehenden Link.
+
+Mit freundlichen Grüßen,
+
+{ADMINNAME} ({ADMINEMAIL})b
+
+----------------------------------------------
+Klicken Du hier um die Umfrage zu starten:
+{SURVEYURL}
+
+Wenn Du an dieser Umfrage nicht teilnehmen und keine weiteren Erinnerungen erhalten möchtest, klicke bitte auf den folgenden Link:
+{OPTOUTURL}', 'Registrierungsbestätigung für Teilnahmeumfrage', 'Hallo {FIRSTNAME},
+
+Du (oder jemand, der Deine E-Mail benutzt hat) hat sich für eine Umfrage mit dem Titel {SURVEYNAME} angemeldet.
+
+Um an dieser Umfrage teilzunehmen, klicke bitte auf den folgenden Link.nn{SURVEYURL}
+
+Wenn Du irgendwelche Fragen zu dieser Umfrage hast oder wenn Du Dich _nicht_ für diese Umfrage angemeldet hast und Du glaubst, dass Dir diese E-Mail irrtümlicherweise zugeschickt worden ist, kontaktiere bitte {ADMINNAME} unter {ADMINEMAIL}.', 'Bestätigung für die Teilnahme an unserer Umfrage', 'Hallo {FIRSTNAME},
+
+Vielen Dank für die Teilnahme an der Umfrage mit dem Titel {SURVEYNAME}. Deine Antworten wurden bei uns gespeichert.
+
+Wenn du irgendwelche Fragen zu dieser E-Mail hast, kontaktiere bitte {ADMINNAME} unter {ADMINEMAIL}.
+
+Mit freundlichen Grüßen,
+
+{ADMINNAME}', 1, 'Antwortabsendung für Umfrage {SURVEYNAME}', 'Hallo,
+
+Eine neue Antwort wurde für die Umfrage ''{SURVEYNAME}'' abgegeben.
+
+Klicke auf den folgenden Link um die Umfrage neu zu laden:
+{RELOADURL}
+
+Klicke auf den folgenden Link um den Antwortdatensatz anzusehen:
+{VIEWRESPONSEURL}
+
+Klicke auf den folgenden Link um den Antwortdatensatz zu bearbeiten:
+{EDITRESPONSEURL}
+
+Um die Statistik zu sehen, klicke hier:
+{STATISTICSURL}', 'Antwortabsendung für Umfrage %s', 'Hallo,
+
+Eine neue Antwort wurde für die Umfrage ''{SURVEYNAME}'' abgegeben.
+
+Klicke auf den folgenden Link um die Umfrage neu zu laden:
+{RELOADURL}
+
+Klicke auf den folgenden Link um den Antwortdatensatz anzusehen:
+{VIEWRESPONSEURL}
+
+Klicke auf den folgenden Link um den Antwortdatensatz zu bearbeiten:
+{EDITRESPONSEURL}
+
+Um die Statistik zu sehen, klicke hier:
+{STATISTICSURL}
+
+
+Die folgenden Antworten wurden vom Teilnehmer gegeben:
+{ANSWERTABLE}', 1 )";
 $parser->process($sql);
 
-$sql = "INSERT INTO surveys ( SID, OWNER_ID, ADMIN, ACTIVE, EXPIRES, STARTDATE, ADMINEMAIL, ANONYMIZED, FAXTO, FORMAT, SAVETIMINGS, TEMPLATE, LANGUAGE, DATESTAMP, USECOOKIE, ALLOWREGISTER, ALLOWSAVE, AUTOREDIRECT, ALLOWPREV, PRINTANSWERS, IPADDR, REFURL, DATECREATED, PUBLICSTATISTICS, PUBLICGRAPHS, LISTPUBLIC, HTMLEMAIL, TOKENANSWERSPERSISTENCE, ASSESSMENTS, USECAPTCHA, BOUNCE_EMAIL, EMAILRESPONSETO, EMAILNOTIFICATIONTO, TOKENLENGTH, SHOWXQUESTIONS, SHOWGROUPINFO, SHOWNOANSWER, SHOWQNUMCODE, SHOWWELCOME, SHOWPROGRESS, ALLOWJUMPS, NAVIGATIONDELAY, NOKEYBOARD, ALLOWEDITAFTERCOMPLETION ) 
-VALUES ( 32225, 1, 'André', 'N', null, null, 'hello@zks.uni-leipzig.de', 'N', '', 'G', 'N', 'default', 'de-informal', 'N', 'N', 'N', 'Y', 'N', 'N', 'N', 'N', 'N', a_function('2012-02-16','YYYY-MM-DD'), 'N', 'N', 'Y', 'Y', 'N', 'N', 'D', 'hello@zks.uni-leipzig.de', '', '', 15, 'Y', 'B', 'Y', 'X', 'Y', 'Y', 'N', 0, 'N', 'N' )";
-$parser->process($sql);
 
-$parser->process(
-        "INSERT INTO users (users_name, password, full_name, parent_id, lang ,email, create_survey,create_user ,delete_user ,superadmin ,configurator ,manage_template , manage_label) VALUES ('admin', to_clob('92e32ca895ca2efd049dcfd79f47b19a6e2dc5f915fbd39e807e6775ae7569c3'), 'Your Name', 0, 'en', 'your-email@example.net', 1,1,1,1,1,1,1)");
 
-$parser->process("INSERT INTO settings_global VALUES ('DBVersion','146')");
-$parser->process(
-        "SELECT a.*, c.*, u.users_name FROM SURVEYS as a  INNER JOIN SURVEYS_LANGUAGESETTINGS as c ON ( surveyls_survey_id = a.sid AND surveyls_language = a.language ) AND surveyls_survey_id=a.sid and surveyls_language=a.language  INNER JOIN USERS as u ON (u.uid=a.owner_id)  ORDER BY surveyls_title");
-$parser->process(
-        " SELECT *, surveyls_title, surveyls_description, surveyls_welcometext, surveyls_url  FROM SURVEYS AS a INNER JOIN SURVEYS_LANGUAGESETTINGS on (surveyls_survey_id=a.sid and surveyls_language=a.language)  order by active DESC, surveyls_title");
+//$sql = "SELECT a.uid, a.users_name FROM USERS AS a LEFT JOIN (SELECT uid AS id FROM USER_IN_GROUPS WHERE ugid = 1) AS b ON a.uid = b.id WHERE id IS NULL ORDER BY a.users_name";
+//$parser->process($sql);
+
+//$sql = "INSERT INTO surveys ( SID, OWNER_ID, ADMIN, ACTIVE, EXPIRES, STARTDATE, ADMINEMAIL, ANONYMIZED, FAXTO, FORMAT, SAVETIMINGS, TEMPLATE, LANGUAGE, DATESTAMP, USECOOKIE, ALLOWREGISTER, ALLOWSAVE, AUTOREDIRECT, ALLOWPREV, PRINTANSWERS, IPADDR, REFURL, DATECREATED, PUBLICSTATISTICS, PUBLICGRAPHS, LISTPUBLIC, HTMLEMAIL, TOKENANSWERSPERSISTENCE, ASSESSMENTS, USECAPTCHA, BOUNCE_EMAIL, EMAILRESPONSETO, EMAILNOTIFICATIONTO, TOKENLENGTH, SHOWXQUESTIONS, SHOWGROUPINFO, SHOWNOANSWER, SHOWQNUMCODE, SHOWWELCOME, SHOWPROGRESS, ALLOWJUMPS, NAVIGATIONDELAY, NOKEYBOARD, ALLOWEDITAFTERCOMPLETION ) 
+//VALUES ( 32225, 1, 'André', 'N', null, null, 'hello@zks.uni-leipzig.de', 'N', '', 'G', 'N', 'default', 'de-informal', 'N', 'N', 'N', 'Y', 'N', 'N', 'N', 'N', 'N', a_function('2012-02-16','YYYY-MM-DD'), 'N', 'N', 'Y', 'Y', 'N', 'N', 'D', 'hello@zks.uni-leipzig.de', '', '', 15, 'Y', 'B', 'Y', 'X', 'Y', 'Y', 'N', 0, 'N', 'N' )";
+//$parser->process($sql);
+
+//$parser->process(
+//        "INSERT INTO users (users_name, password, full_name, parent_id, lang ,email, create_survey,create_user ,delete_user ,superadmin ,configurator ,manage_template , manage_label) VALUES ('admin', to_clob('92e32ca895ca2efd049dcfd79f47b19a6e2dc5f915fbd39e807e6775ae7569c3'), 'Your Name', 0, 'en', 'your-email@example.net', 1,1,1,1,1,1,1)");
+
+//$parser->process("INSERT INTO settings_global VALUES ('DBVersion','146')");
+//$parser->process(
+//        "SELECT a.*, c.*, u.users_name FROM SURVEYS as a  INNER JOIN SURVEYS_LANGUAGESETTINGS as c ON ( surveyls_survey_id = a.sid AND surveyls_language = a.language ) AND surveyls_survey_id=a.sid and surveyls_language=a.language  INNER JOIN USERS as u ON (u.uid=a.owner_id)  ORDER BY surveyls_title");
+//$parser->process(
+//        " SELECT *, surveyls_title, surveyls_description, surveyls_welcometext, surveyls_url  FROM SURVEYS AS a INNER JOIN SURVEYS_LANGUAGESETTINGS on (surveyls_survey_id=a.sid and surveyls_language=a.language)  order by active DESC, surveyls_title");
 
 //$parser->process("CREATE TABLE answers ( qid number(11) default '0' NOT NULL, code varchar2(5) default '' NOT NULL, answer CLOB NOT NULL, assessment_value number(11) default '0' NOT NULL, sortorder number(11) NOT NULL, language varchar2(20) default 'en', scale_id number(3) default '0' NOT NULL, PRIMARY KEY (qid,code,language,scale_id) )");
 //$parser->process("USE DATABASE `sdbprod`");
-$parser->process("insert into SETTINGS_GLOBAL (stg_value,stg_name) values('','force_ssl')");
+//$parser->process("insert into SETTINGS_GLOBAL (stg_value,stg_name) values('','force_ssl')");
 
 //$parser->process("SELECT * FROM SETTINGS_GLOBAL");
 //$parser->process("SELECT stg_value FROM SETTINGS_GLOBAL where stg_name='force_ssl'");
