1665da3f50ac2f44cf34aad46403566e18b25912
Andre Rothe <phosco@gmx.de>
2012-03-13 11:35:22 +0000

    ADD: new test for issue 36 (parser and creator)
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@181 44dcfd11-89b7-3d55-2063-5c365055f77f

--- t/issue36.php
+++ t/issue36.php
@@ -3,6 +3,7 @@ require_once(dirname(__FILE__) . '/../php-sql-parser.php');
 require_once(dirname(__FILE__) . '/../test-more.php');
 
 $parser = new PHPSQLParser();
+
 $sql = "INSERT INTO test (`name`, `test`) VALUES ('\'Superman\'', ''), ('\'Superman\'', '')";
 $parser->parse($sql);
 $p = $parser->parsed;
@@ -10,9 +11,15 @@ $expected = getExpectedValue('issue36a.serialized');
 eq_array($p, $expected, 'INSERT statement with escaped quotes and multiple records');
 
 
-$parser = new PHPSQLParser();
 $sql = "INSERT INTO test (`name`, `test`) VALUES ('\'Superman\'', '')";
 $parser->parse($sql);
 $p = $parser->parsed;
 $expected = getExpectedValue('issue36b.serialized');
 eq_array($p, $expected, 'INSERT statement with escaped quotes and one record');
+
+
+$sql = "INSERT INTO test (`name`, `test`) VALUES ('\'Superman\'', ''), ('\'sdfsd\'', '')";
+$parser->parse($sql);
+$p = $parser->parsed;
+$expected = getExpectedValue('issue36c.serialized');
+eq_array($p, $expected, 'INSERT statement with escaped quotes and multiple records (2)');
