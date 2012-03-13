6f80757858260ae4df525a28e6d3c4a6e137f56f
Andre Rothe <phosco@gmx.de>
2012-03-13 10:29:42 +0000

    execution order of the test scripts have been reordered.
    git-svn-id: http://php-sql-parser.googlecode.com/svn/trunk@120 44dcfd11-89b7-3d55-2063-5c365055f77f

--- tests-parser.php
+++ tests-parser.php
@@ -3,23 +3,24 @@
 /**
  * execute all tests
  */
+require_once(dirname(__FILE__) . '/t/aliases.php');
 require_once(dirname(__FILE__) . '/t/allcolumns.php');
-require_once(dirname(__FILE__) . '/t/union.php');
-require_once(dirname(__FILE__) . '/t/positions.php');
-require_once(dirname(__FILE__) . '/t/subselect.php');
+require_once(dirname(__FILE__) . '/t/backtick.php');
+require_once(dirname(__FILE__) . '/t/delete.php');
+require_once(dirname(__FILE__) . '/t/from.php');
+require_once(dirname(__FILE__) . '/t/gtltop.php');
+require_once(dirname(__FILE__) . '/t/insert.php');
 require_once(dirname(__FILE__) . '/t/issue12.php');
 require_once(dirname(__FILE__) . '/t/issue15.php');
 require_once(dirname(__FILE__) . '/t/issue21.php');
 require_once(dirname(__FILE__) . '/t/issue25.php');
-require_once(dirname(__FILE__) . '/t/aliases.php');
-require_once(dirname(__FILE__) . '/t/backtick.php');
-require_once(dirname(__FILE__) . '/t/from.php');
-require_once(dirname(__FILE__) . '/t/gtltop.php');
 require_once(dirname(__FILE__) . '/t/left.php');
 require_once(dirname(__FILE__) . '/t/nested.php');
+require_once(dirname(__FILE__) . '/t/positions.php');
 require_once(dirname(__FILE__) . '/t/select.php');
+require_once(dirname(__FILE__) . '/t/subselect.php');
+require_once(dirname(__FILE__) . '/t/union.php');
 require_once(dirname(__FILE__) . '/t/update.php');
 require_once(dirname(__FILE__) . '/t/zero.php');
-require_once(dirname(__FILE__) . '/t/insert.php');
 
 exit;
