032c24b07ddd24814f25f11407fa5e877b93352c
Andre Rothe <phosco@gmx.de>
2012-03-13 11:51:54 +0000

    The directories "t" and "r" have been renamed to "tests" and "expected"
    git-svn-id: http://php-sql-parser.googlecode.com/svn/trunk@183 44dcfd11-89b7-3d55-2063-5c365055f77f

--- tests-parser.php
+++ tests-parser.php
@@ -3,31 +3,31 @@
 /**
  * execute all tests
  */
-require_once(dirname(__FILE__) . '/t/aliases.php');
-require_once(dirname(__FILE__) . '/t/allcolumns.php');
-require_once(dirname(__FILE__) . '/t/backtick.php');
-require_once(dirname(__FILE__) . '/t/delete.php');
-require_once(dirname(__FILE__) . '/t/from.php');
-require_once(dirname(__FILE__) . '/t/gtltop.php');
-require_once(dirname(__FILE__) . '/t/inlist.php');
-require_once(dirname(__FILE__) . '/t/insert.php');
-require_once(dirname(__FILE__) . '/t/issue11.php');
-require_once(dirname(__FILE__) . '/t/issue12.php');
-require_once(dirname(__FILE__) . '/t/issue15.php');
-require_once(dirname(__FILE__) . '/t/issue21.php');
-require_once(dirname(__FILE__) . '/t/issue25.php');
-require_once(dirname(__FILE__) . '/t/issue30.php');
-require_once(dirname(__FILE__) . '/t/issue31.php');
-require_once(dirname(__FILE__) . '/t/issue32.php');
-require_once(dirname(__FILE__) . '/t/issue34.php');
-require_once(dirname(__FILE__) . '/t/issue36.php');
-require_once(dirname(__FILE__) . '/t/left.php');
-require_once(dirname(__FILE__) . '/t/nested.php');
-require_once(dirname(__FILE__) . '/t/positions.php');
-require_once(dirname(__FILE__) . '/t/select.php');
-require_once(dirname(__FILE__) . '/t/subselect.php');
-require_once(dirname(__FILE__) . '/t/union.php');
-require_once(dirname(__FILE__) . '/t/update.php');
-require_once(dirname(__FILE__) . '/t/zero.php');
+require_once(dirname(__FILE__) . '/tests/aliases.php');
+require_once(dirname(__FILE__) . '/tests/allcolumns.php');
+require_once(dirname(__FILE__) . '/tests/backtick.php');
+require_once(dirname(__FILE__) . '/tests/delete.php');
+require_once(dirname(__FILE__) . '/tests/from.php');
+require_once(dirname(__FILE__) . '/tests/gtltop.php');
+require_once(dirname(__FILE__) . '/tests/inlist.php');
+require_once(dirname(__FILE__) . '/tests/insert.php');
+require_once(dirname(__FILE__) . '/tests/issue11.php');
+require_once(dirname(__FILE__) . '/tests/issue12.php');
+require_once(dirname(__FILE__) . '/tests/issue15.php');
+require_once(dirname(__FILE__) . '/tests/issue21.php');
+require_once(dirname(__FILE__) . '/tests/issue25.php');
+require_once(dirname(__FILE__) . '/tests/issue30.php');
+require_once(dirname(__FILE__) . '/tests/issue31.php');
+require_once(dirname(__FILE__) . '/tests/issue32.php');
+require_once(dirname(__FILE__) . '/tests/issue34.php');
+require_once(dirname(__FILE__) . '/tests/issue36.php');
+require_once(dirname(__FILE__) . '/tests/left.php');
+require_once(dirname(__FILE__) . '/tests/nested.php');
+require_once(dirname(__FILE__) . '/tests/positions.php');
+require_once(dirname(__FILE__) . '/tests/select.php');
+require_once(dirname(__FILE__) . '/tests/subselect.php');
+require_once(dirname(__FILE__) . '/tests/union.php');
+require_once(dirname(__FILE__) . '/tests/update.php');
+require_once(dirname(__FILE__) . '/tests/zero.php');
 
 exit;
