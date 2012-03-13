39ee8bf90697474519016c013a4b557cc8c47242
Andre Rothe <phosco@gmx.de>
2012-03-13 10:36:05 +0000

    issue 12 has been solved!
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@124 44dcfd11-89b7-3d55-2063-5c365055f77f

--- t/issue12.php
+++ t/issue12.php
@@ -2,12 +2,9 @@
 require_once(dirname(__FILE__) . "/../php-sql-parser.php");
 require_once(dirname(__FILE__) . "/../test-more.php");
 
-# not solved
-
 $parser = new PHPSQLParser();
 
 $sql = "SELECT SQL_CALC_FOUND_ROWS SmTable.*, MATCH (SmTable.fulltextsearch_keyword) AGAINST ('google googles') AS keyword_score FROM SmTable WHERE SmTable.status = 'A' AND (SmTable.country_id = 1 AND SmTable.state_id = 10) AND MATCH (SmTable.fulltextsearch_keyword) AGAINST ('google googles') ORDER BY SmTable.level DESC, keyword_score DESC LIMIT 0,10";
 $p = $parser->parse($sql);
-
 $expected = getExpectedValue('issue12.serialized');
 eq_array($p, $expected, 'issue 12');
