26778cdf2123fd827bbbaa4e7021cd9678cda615
Andre Rothe <phosco@gmx.de>
2012-03-13 08:22:55 +0000

    The update test goes wrong, because there is a mssing ")" on the deepest sub-query.
    git-svn-id: http://php-sql-parser.googlecode.com/svn/trunk@50 44dcfd11-89b7-3d55-2063-5c365055f77f

--- t/update.php
+++ t/update.php
@@ -4,8 +4,9 @@ require_once(dirname(__FILE__) . '/../test-more.php');
 
 $parser = new PHPSQLParser();
 
-$sql = "UPDATE table1 SET field1='foo' WHERE field2='bar' AND id=(SELECT if FROM test1 t where t.field1=(SELECT id from test2 t2 where t2.field = 'foo'))";
+$sql = "UPDATE table1 SET field1='foo' WHERE field2='bar' AND id=(SELECT id FROM test1 t where t.field1=(SELECT id from test2 t2 where t2.field = 'foo'))";
 $parser->parse($sql);
 $p = $parser->parsed;
-$expected = getExpected('update.serialized');
+
+$expected = getExpectedValue('update.serialized');
 eq_array($p, $expected, 'update with a sub-select');
