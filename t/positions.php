ec513ab583a18b3232ab98435e1b756cdc2f9d04
Andre Rothe <phosco@gmx.de>
2012-03-13 09:06:02 +0000

    Some extensions for the position matching, the  base_expr can be surrounded by arithemtic operators like = or <=. But if the base_expr is an arithmetic operator, it must be surronded by space characters. This case is not useable, we have to enhance the code!
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@77 44dcfd11-89b7-3d55-2063-5c365055f77f

--- t/positions.php
+++ t/positions.php
@@ -17,3 +17,7 @@ ok($p['SELECT'][0]['position'] == 7, 'position of column');
 ok($p['SELECT'][0]['alias']['position'] == 12, 'position of column alias');
 ok($p['FROM'][0]['position'] == 23, 'position of table');
 ok($p['FROM'][0]['alias']['position'] == 28, 'position of table alias');
+
+# this goes wrong with the
+$sql = "SELECT a.*, c.*, u.users_name FROM SURVEYS as a  INNER JOIN SURVEYS_LANGUAGESETTINGS as c ON ( surveyls_survey_id = a.sid AND surveyls_language = a.language ) AND surveyls_survey_id=a.sid and surveyls_language=a.language  INNER JOIN USERS as u ON (u.uid=a.owner_id)  ORDER BY surveyls_title";
+$p = $parser->parse($sql, true);
