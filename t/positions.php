684fdd01da7c782d202b1aa2949dc8b04697b795
Andre Rothe <phosco@gmx.de>
2012-03-13 09:08:47 +0000

    The test for the wrong position clause. There are some problems within lookForBaseExpression().
    git-svn-id: http://php-sql-parser.googlecode.com/svn/trunk@80 44dcfd11-89b7-3d55-2063-5c365055f77f

--- t/positions.php
+++ t/positions.php
@@ -18,6 +18,8 @@ ok($p['SELECT'][0]['alias']['position'] == 12, 'position of column alias');
 ok($p['FROM'][0]['position'] == 23, 'position of table');
 ok($p['FROM'][0]['alias']['position'] == 28, 'position of table alias');
 
-# this goes wrong with the
+# this goes wrong (no delimiter between colref,operator,colref 
 $sql = "SELECT a.*, c.*, u.users_name FROM SURVEYS as a  INNER JOIN SURVEYS_LANGUAGESETTINGS as c ON ( surveyls_survey_id = a.sid AND surveyls_language = a.language ) AND surveyls_survey_id=a.sid and surveyls_language=a.language  INNER JOIN USERS as u ON (u.uid=a.owner_id)  ORDER BY surveyls_title";
 $p = $parser->parse($sql, true);
+$expected = getExpectedValue('positions1.serialized');
+eq_array($p, $expected, 'a long query with join and order clauses');
