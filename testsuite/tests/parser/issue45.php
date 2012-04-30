128c466359f59e8f4ac49f989c12df5e51c60137
Andre Rothe <phosco@gmx.de>
2012-04-30 12:49:06 +0000

    CNG: tests of issue 45 and 56 have been corrected. issue 45 is still unsolved.
    git-svn-id: http://php-sql-parser.googlecode.com/svn/trunk@251 44dcfd11-89b7-3d55-2063-5c365055f77f

--- testsuite/tests/parser/issue45.php
+++ testsuite/tests/parser/issue45.php
@@ -10,13 +10,8 @@ $sql = 'SELECT a from b left join c on c.a = b.a and (c.b. = b.b) where a.a > 1'
 $parser->parse($sql, false);
 $p = $parser->parsed;
 
-print_r($p);
-
-
 $parser->parse($sql, true);
 $p = $parser->parsed;
-
-print_r($p);
 
 $expected = getExpectedValue(dirname(__FILE__), 'issue45.serialized');
 eq_array($p, $expected, 'issue 45 position problem');
--- testsuite/tests/parser/issue46.php
+++ testsuite/tests/parser/issue46.php
@@ -2,16 +2,18 @@
 require_once(dirname(__FILE__) . '/../../../php-sql-parser.php');
 require_once(dirname(__FILE__) . '/../../test-more.php');
 
-$parser = new PHPSQLParser();
+$testname = "issue 46, throws exception on error";
+try {
 
-// not solved
-
-$sql = 'SELECT [CONCAT(a, b)] from c';
-$parser->parse($sql, false);
-$p = $parser->parsed;
-
-print_r($p);
-
-$expected = getExpectedValue(dirname(__FILE__), 'issue44.serialized');
-eq_array($p, $expected, 'issue 44 position problem');
-
+    $parser = new PHPSQLParser();
+    $sql = "SELECT abc'haha'";  // test code from issue doesn't longer create an exception
+    $parser->parse($sql, true);
+    
+    $p = $parser->parsed;
+    print_r($p);
+        
+    fail($testname);
+    
+} catch (UnableToCalculatePositionException $e) {
+    ok(true, $testname);
+}
