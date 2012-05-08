2ce017c1f6320aee68e0880ddbb978ac574ba534
Andre Rothe <phosco@gmx.de>
2012-05-08 14:34:27 +0000

    ADD: test case for the issue 57.
    git-svn-id: http://php-sql-parser.googlecode.com/svn/trunk@273 44dcfd11-89b7-3d55-2063-5c365055f77f

--- testsuite/tests-creator.php
+++ testsuite/tests-creator.php
@@ -8,6 +8,7 @@ require_once(dirname(__FILE__) . '/tests/creator/count_distinct.php');
 require_once(dirname(__FILE__) . '/tests/creator/function.php');
 require_once(dirname(__FILE__) . '/tests/creator/inlist.php');
 require_once(dirname(__FILE__) . '/tests/creator/insert.php');
+require_once(dirname(__FILE__) . '/tests/creator/issue57.php');
 require_once(dirname(__FILE__) . '/tests/creator/join.php');
 require_once(dirname(__FILE__) . '/tests/creator/left.php');
 require_once(dirname(__FILE__) . '/tests/creator/tableexpr.php');
