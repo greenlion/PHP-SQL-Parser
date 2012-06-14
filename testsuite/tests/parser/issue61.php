738d8ebf3ad23dd0f86f51aeb8720041a65e1066
Andre Rothe <phosco@gmx.de>
2012-06-14 06:33:37 +0000

    FIX: issue 61
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@286 44dcfd11-89b7-3d55-2063-5c365055f77f

--- testsuite/tests/parser/issue61.php
+++ testsuite/tests/parser/issue61.php
@@ -2,12 +2,8 @@
 require_once(dirname(__FILE__) . '/../../../php-sql-parser.php');
 require_once(dirname(__FILE__) . '/../../test-more.php');
 
-// parser can not handle functions or expressions within order-by
-// it seems to be a problem since REV 142
-
 $sql = "SELECT lcase(dummy.b) FROM dummy ORDER BY dummy.a, LCASE(dummy.b)";
 $parser = new PHPSQLParser($sql);
 $p = $parser->parsed;
-
-print_r($p);
-echo serialize($p);
+$expected = getExpectedValue(dirname(__FILE__), 'issue61.serialized');
+eq_array($p, $expected, 'functions/expressions within ORDER-BY');
