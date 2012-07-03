24749b70426c14778b082f657b07ab929a72837c
Andre Rothe <phosco@gmx.de>
2012-07-03 08:28:18 +0000

    REM: methods startsWith() and changeCaseForArrayValues() of PHPSQLParserUtils class are dead code.
    git-svn-id: http://php-sql-parser.googlecode.com/svn/trunk@292 44dcfd11-89b7-3d55-2063-5c365055f77f

--- php-sql-parser.php
+++ php-sql-parser.php
@@ -162,47 +162,6 @@ if (!defined('HAVE_PHP_SQL_PARSER')) {
         }
 
         /**
-         * 
-         * Change the case of the values of an array.
-         * 
-         * @param Array of Strings $arr
-         * @param unknown_type $case (CASE_LOWER or CASE_UPPER)
-         * @throws InvalidArgumentException if the first argument is not an array
-         */
-        protected function changeCaseForArrayValues($arr, $case) {
-            if (!is_array($arr)) {
-                throw new InvalidArgumentException("first argument must be an array");
-            }
-            for ($i = 0; $i < count($arr); ++$i) {
-                if ($case == CASE_LOWER) {
-                    $arr[$i] = strtolower($arr[$i]);
-                }
-                if ($case == CASE_UPPER) {
-                    $arr[$i] = strtoupper($arr[$i]);
-                }
-            }
-            return $arr;
-        }
-
-        /**
-         * Starts one of the strings within the given array $haystack with the string $needle?
-         * @param string or array $haystack
-         * @param string $needle
-         */
-        protected function startsWith($haystack, $needle) {
-            if (is_string($needle)) {
-                $needle = array($needle);
-            }
-            for ($j = 0; $j < count($needle); ++$j) {
-                $length = strlen($needle[$j]);
-                if (substr($haystack, 0, $length) === $needle[$j]) {
-                    return $j;
-                }
-            }
-            return false;
-        }
-
-        /**
          * Ends the given string $haystack with the string $needle?
          * @param string $haystack
          * @param string $needle
