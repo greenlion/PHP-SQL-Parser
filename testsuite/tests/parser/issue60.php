6579a7a98e7cbce1a8a7819208f8d2525ce9b569
Andre Rothe <phosco@gmx.de>
2012-08-30 06:53:30 +0000

    ADD: test for issue 60, password should be a colref instead of a function
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@344 44dcfd11-89b7-3d55-2063-5c365055f77f

--- testsuite/tests/parser/issue60.php
+++ testsuite/tests/parser/issue60.php
@@ -2,10 +2,10 @@
 require_once(dirname(__FILE__) . '/../../../php-sql-parser.php');
 require_once(dirname(__FILE__) . '/../../test-more.php');
 
-# the resultset defines password as function instead of colref
-
+# not solved: the resultset defines password as function instead of colref
 $sql = "SELECT id, password
 FROM users";
 $parser = new PHPSQLParser($sql, true);
 $p = $parser->parsed;
-ok($p['SELECT'][1]['position'] == 11, 'wrong usage of keyword password');
+ok($p['SELECT'][1]['position'] === 11, 'wrong usage of keyword password');
+ok($p['SELECT'][1]['expr_type'] === 'colref', 'password should be a colref here');
