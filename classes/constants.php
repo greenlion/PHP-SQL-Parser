5f21e3ebfea3970c7c479d28eb092a53dd2168ba
Andre Rothe <phosco@gmx.de>
2012-08-30 06:41:26 +0000

    FIX: issue 62, add DATETIME as reserved word
    git-svn-id: http://php-sql-parser.googlecode.com/svn/trunk@343 44dcfd11-89b7-3d55-2063-5c365055f77f

--- classes/constants.php
+++ classes/constants.php
@@ -38,7 +38,7 @@ class PHPSQLParserConstants {
                                        'CHARACTER_LENGTH', 'CHARSET', 'COALESCE', 'COERCIBILITY', 'COLLATION',
                                        'COMPRESS', 'CONCAT', 'CONCAT_WS', 'CONECTION_ID', 'CONV', 'CONVERT',
                                        'CONVERT_TZ', 'COS', 'COT', 'COUNT', 'CRC32', 'CURDATE', 'CURRENT_USER',
-                                       'CURRVAL', 'CURTIME', 'DATABASE', 'DATE_ADD', 'DATE_DIFF', 'DATE_FORMAT',
+                                       'CURRVAL', 'CURTIME', 'DATABASE', 'DATETIME', 'DATE_ADD', 'DATE_DIFF', 'DATE_FORMAT',
                                        'DATE_SUB', 'DAY', 'DAYNAME', 'DAYOFMONTH', 'DAYOFWEEK', 'DAYOFYEAR', 'DECODE',
                                        'DEFAULT', 'DEGREES', 'DES_DECRYPT', 'DES_ENCRYPT', 'ELT', 'ENCODE', 'ENCRYPT',
                                        'EXP', 'EXPORT_SET', 'EXTRACT', 'FIELD', 'FIND_IN_SET', 'FLOOR', 'FORMAT',
