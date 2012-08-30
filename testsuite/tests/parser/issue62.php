857153f7ef5cf6ef218efe2463651f05815ecae9
Andre Rothe <phosco@gmx.de>
2012-08-30 06:41:26 +0000

    FIX: issue 62, add DATETIME as reserved word
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@343 44dcfd11-89b7-3d55-2063-5c365055f77f

--- testsuite/tests/parser/issue62.php
+++ testsuite/tests/parser/issue62.php
@@ -2,7 +2,6 @@
 require_once(dirname(__FILE__) . '/../../../php-sql-parser.php');
 require_once(dirname(__FILE__) . '/../../test-more.php');
 
-// TODO: not solved, DATETIME is recognized as colref
 $sql = "SELECT CAST((CONCAT(table1.col1,' ',time_start)) AS DATETIME) FROM table1";
 $parser = new PHPSQLParser($sql,true);
 $p = $parser->parsed;
