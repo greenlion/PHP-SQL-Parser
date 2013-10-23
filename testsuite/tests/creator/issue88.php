e6220e9ee191582b51da5b8f34897a2cde3d89ac
Andre Rothe <phosco@gmx.de>
2013-10-23 08:22:05 +0000

    ADD: a new test for issue 88
    git-svn-id: http://php-sql-parser.googlecode.com/svn/trunk@349 44dcfd11-89b7-3d55-2063-5c365055f77f

---
+++ testsuite/tests/creator/issue88.php
@@ -0,0 +1,12 @@
+<?php
+require_once(dirname(__FILE__) . '/../../../php-sql-parser.php');
+require_once(dirname(__FILE__) . '/../../../php-sql-creator.php');
+require_once(dirname(__FILE__) . '/../../test-more.php');
+
+
+$sql = "select (some_field = 'string') from table;";
+$parser = new PHPSQLParser($sql, true);
+$creator = new PHPSQLCreator($parser->parsed);
+$created = $creator->created;
+$expected = getExpectedValue(dirname(__FILE__), 'issue88.sql', false);
+ok($created === $expected, 'Expression subtree should handle bracket_expressions.');
