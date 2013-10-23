f1ad3d07952d3f4594a3155af67f7a21e6d9aaf6
Andre Rothe <phosco@gmx.de>
2013-10-23 09:07:10 +0000

    ADD: a new test for issue 85
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@357 44dcfd11-89b7-3d55-2063-5c365055f77f

---
+++ testsuite/tests/creator/issue85.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once(dirname(__FILE__) . '/../../../php-sql-parser.php');
+require_once(dirname(__FILE__) . '/../../../php-sql-creator.php');
+require_once(dirname(__FILE__) . '/../../test-more.php');
+
+$sql = "SELECT haha(foo, bar);";
+$parser = new PHPSQLParser($sql);
+$creator = new PHPSQLCreator($parser->parsed);
+$created = $creator->created;
+$expected = getExpectedValue(dirname(__FILE__), 'issue85.sql', false);
+ok($created === $expected, 'SELECT statements without FROM (wtf?)');
