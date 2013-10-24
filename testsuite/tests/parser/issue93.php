2268866d99185dc7e88738bad6902d2706e8a7f3
Andre Rothe <phosco@gmx.de>
2013-10-24 20:30:33 +0000

    CHG: some test cases have been corrected, some exception handlers have been added. Test suite runs till the end, there are 7 invalid tests for some unsolved issues.
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@373 44dcfd11-89b7-3d55-2063-5c365055f77f

--- testsuite/tests/parser/issue93.php
+++ testsuite/tests/parser/issue93.php
@@ -2,11 +2,13 @@
 require_once(dirname(__FILE__) . '/../../../php-sql-parser.php');
 require_once(dirname(__FILE__) . '/../../test-more.php');
 
+# TODO: not solved
 $sql="select 1 as `a` order by `a`";
-$parser = new PHPSQLParser($sql, true);
+
+try {
+	$parser = new PHPSQLParser($sql, true);
+} catch (UnableToCalculatePositionException $e) {}
+
 $p = $parser->parsed;
-
-print_r($p);
-
 $expected = getExpectedValue(dirname(__FILE__), 'issue93.serialized');
 eq_array($p, $expected, 'simple query');
--- testsuite/tests/parser/issue97.php
+++ testsuite/tests/parser/issue97.php
@@ -2,7 +2,6 @@
 require_once(dirname(__FILE__) . '/../../../php-sql-parser.php');
 require_once(dirname(__FILE__) . '/../../test-more.php');
 
-# incomplete floating point numbers 
 $sql = "select webid, floor(iz/2.) as fl from MDR1.Tweb512 as w where w.webid < 100";
 $parser = new PHPSQLParser($sql);
 $p = $parser->parsed;
