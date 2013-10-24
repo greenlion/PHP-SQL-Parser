16f0971b2c78b49883928614ec6bd468db93fabf
Andre Rothe <phosco@gmx.de>
2013-10-24 22:49:45 +0000

    ADD: a test for issue 94 FIX: issue 94
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@381 44dcfd11-89b7-3d55-2063-5c365055f77f

---
+++ testsuite/tests/creator/issue94.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once(dirname(__FILE__) . '/../../../php-sql-parser.php');
+require_once(dirname(__FILE__) . '/../../../php-sql-creator.php');
+require_once(dirname(__FILE__) . '/../../test-more.php');
+
+$sql = 'SELECT DATE_ADD(NOW(), INTERVAL 1 MONTH) AS next_month';
+$parser = new PHPSQLParser($sql);
+$creator = new PHPSQLCreator($parser->parsed);
+$created = $creator->created;
+$expected = getExpectedValue(dirname(__FILE__), 'issue94.sql', false);
+ok($created === $expected, 'creating date_add with interval');
