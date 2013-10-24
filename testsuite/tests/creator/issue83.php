6f4d288a5f0d35f423b899a166780e045fb001ea
Andre Rothe <phosco@gmx.de>
2013-10-24 21:12:57 +0000

    ADD: an output for issue 83, it isn't solved yet.
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@376 44dcfd11-89b7-3d55-2063-5c365055f77f

--- testsuite/tests/creator/issue83.php
+++ testsuite/tests/creator/issue83.php
@@ -4,13 +4,10 @@ require_once(dirname(__FILE__) . '/../../../php-sql-parser.php');
 require_once(dirname(__FILE__) . '/../../../php-sql-creator.php');
 require_once(dirname(__FILE__) . '/../../test-more.php');
 
+#TODO: not solved, the parser doesn't handle subselects here
 $sql = "INSERT INTO newTablename (SELECT * FROM oldTablename);";
 $parser = new PHPSQLParser($sql, true);
-print_r($parser->parsed);
-
 $creator = new PHPSQLCreator($parser->parsed);
 $created = $creator->created;
-print_r($created);
-
 $expected = getExpectedValue(dirname(__FILE__), 'issue83.sql', false);
 ok($created === $expected, 'Subselect instead of value list.');
