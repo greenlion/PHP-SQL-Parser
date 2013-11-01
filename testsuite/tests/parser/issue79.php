45e52514750b45b693d41ea518cd7dacb31b53b6
Andre Rothe <phosco@gmx.de>
2013-11-01 16:17:28 +0000

    ADD: a test for user variables with assignment
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@389 44dcfd11-89b7-3d55-2063-5c365055f77f

---
+++ testsuite/tests/parser/issue79.php
@@ -0,0 +1,17 @@
+<?php
+require_once(dirname(__FILE__) . '/../../../php-sql-parser.php');
+require_once(dirname(__FILE__) . '/../../test-more.php');
+
+$sql = "SELECT * FROM `users` WHERE id_user=@ID_USER";
+$parser = new PHPSQLParser($sql);
+$p = $parser->parsed;
+$expected = getExpectedValue(dirname(__FILE__), 'issue79a.serialized');
+eq_array($p, $expected, 'user variables');
+
+$sql = "SELECT (@aa:=id) AS a, (@aa+3) AS b FROM tbl_name HAVING b=5";
+$parser = new PHPSQLParser($sql);
+$p = $parser->parsed;
+print_r($p);
+$expected = getExpectedValue(dirname(__FILE__), 'issue79b.serialized');
+eq_array($p, $expected, 'user variables with alias and assignment');
+
