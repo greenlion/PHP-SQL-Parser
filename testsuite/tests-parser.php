a5a950986003afa355cb61a0f9e2329dd30b8ab3
Andre Rothe <phosco@gmx.de>
2013-11-01 15:39:59 +0000

    ADD: a test case for issue 80
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@384 44dcfd11-89b7-3d55-2063-5c365055f77f

--- testsuite/tests-parser.php
+++ testsuite/tests-parser.php
@@ -50,6 +50,7 @@ require_once(dirname(__FILE__) . '/tests/parser/issue70.php');
 require_once(dirname(__FILE__) . '/tests/parser/issue71.php');
 require_once(dirname(__FILE__) . '/tests/parser/issue72.php');
 require_once(dirname(__FILE__) . '/tests/parser/issue74.php');
+require_once(dirname(__FILE__) . '/tests/parser/issue80.php');
 require_once(dirname(__FILE__) . '/tests/parser/issue82.php');
 require_once(dirname(__FILE__) . '/tests/parser/issue91.php');
 require_once(dirname(__FILE__) . '/tests/parser/issue93.php');
