dfa0d8016c79c70aa3a38a331fc8ed9668fb2826
Andre Rothe <phosco@gmx.de>
2013-11-01 20:06:34 +0000

    CHG: some option keywords like DISTINCT have been moved into the SELECT category. The parser recognize such words as reserved now. FIX: issue 91
    git-svn-id: http://php-sql-parser.googlecode.com/svn/trunk@391 44dcfd11-89b7-3d55-2063-5c365055f77f

--- testsuite/tests/parser/issue91.php
+++ testsuite/tests/parser/issue91.php
@@ -2,10 +2,9 @@
 require_once(dirname(__FILE__) . "/../../../php-sql-parser.php");
 require_once(dirname(__FILE__) . "/../../test-more.php");
 
-$parser = new PHPSQLParser();
 
-# TODO: not solved
 $sql = 'SELECT DISTINCT colA * colB From test t';
+$parser = new PHPSQLParser();
 $p = $parser->parse($sql);
 $expected = getExpectedValue(dirname(__FILE__), 'issue91.serialized');
 eq_array($p, $expected, 'distinct select');
