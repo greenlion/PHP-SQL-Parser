66a70bf180f1ee815c277373ec3054c958a57507
Andre Rothe <phosco@gmx.de>
2013-11-01 16:20:25 +0000

    CHG: test of issue 38 has been enhanced.
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@390 44dcfd11-89b7-3d55-2063-5c365055f77f

--- testsuite/tests/parser/issue38.php
+++ testsuite/tests/parser/issue38.php
@@ -2,10 +2,11 @@
 require_once(dirname(__FILE__) . '/../../../php-sql-parser.php');
 require_once(dirname(__FILE__) . '/../../test-more.php');
 
-$parser = new PHPSQLParser();
-
+# TODO: there are warnings within the parser
 $sql = "SELECT * FROM `table` `t` WHERE ( ( UNIX_TIMESTAMP() + 3600 ) > `t`.`expires` ) ";
+$parser = new PHPSQLParser();
 $parser->parse($sql);
 $p = $parser->parsed;
+print_r($p);
 $expected = getExpectedValue(dirname(__FILE__), 'issue38.serialized');
 eq_array($p, $expected, 'function within WHERE and quoted table + quoted columns');
