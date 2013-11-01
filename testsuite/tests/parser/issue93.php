df1abcf8b3670f2de0394e6797e4ee8855726893
Andre Rothe <phosco@gmx.de>
2013-11-01 15:45:15 +0000

    FIX: issue 93, it was the same as issue 80.
    git-svn-id: http://php-sql-parser.googlecode.com/svn/trunk@386 44dcfd11-89b7-3d55-2063-5c365055f77f

--- testsuite/tests/parser/issue93.php
+++ testsuite/tests/parser/issue93.php
@@ -2,13 +2,8 @@
 require_once(dirname(__FILE__) . '/../../../php-sql-parser.php');
 require_once(dirname(__FILE__) . '/../../test-more.php');
 
-# TODO: not solved
 $sql="select 1 as `a` order by `a`";
-
-try {
-	$parser = new PHPSQLParser($sql, true);
-} catch (UnableToCalculatePositionException $e) {}
-
+$parser = new PHPSQLParser($sql, true);
 $p = $parser->parsed;
 $expected = getExpectedValue(dirname(__FILE__), 'issue93.serialized');
 eq_array($p, $expected, 'simple query');
