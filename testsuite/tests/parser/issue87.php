df7a6578069fc5d7541cb4f12f22b8bfebd77606
Andre Rothe <phosco@gmx.de>
2013-11-02 09:41:55 +0000

    ADD: a test case for issue 87 (RENAME TABLE)
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@397 44dcfd11-89b7-3d55-2063-5c365055f77f

---
+++ testsuite/tests/parser/issue87.php
@@ -0,0 +1,17 @@
+<?php
+require_once(dirname(__FILE__) . '/../../../php-sql-parser.php');
+require_once(dirname(__FILE__) . '/../../test-more.php');
+
+$parser = new PHPSQLParser();
+$sql = "RENAME TABLE a TO b";
+$parser->parse($sql, true);
+$p = $parser->parsed;
+$expected = getExpectedValue(dirname(__FILE__), 'issue87a.serialized');
+eq_array($p, $expected, 'rename table');
+
+$parser = new PHPSQLParser();
+$sql = "RENAME TABLE a TO b, `c` to `a`, foo.bar to hello.world";
+$parser->parse($sql, true);
+$p = $parser->parsed;
+$expected = getExpectedValue(dirname(__FILE__), 'issue87b.serialized');
+eq_array($p, $expected, 'rename multiple tables');
