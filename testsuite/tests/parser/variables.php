318adbf72f01297ea151b3a5c0355bec7e787d8b
Andre Rothe <phosco@gmx.de>
2013-11-04 15:55:15 +0000

    ADD: some test code to check assigmentsof variables like SELECT (@a:=@b+3) FROM table.
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@410 44dcfd11-89b7-3d55-2063-5c365055f77f

---
+++ testsuite/tests/parser/variables.php
@@ -0,0 +1,15 @@
+<?php
+require_once(dirname(__FILE__) . '/../../../php-sql-parser.php');
+require_once(dirname(__FILE__) . '/../../test-more.php');
+
+$sql = "SELECT @t1, @`t2`, @t3, @t4 := @t1+@'t2'+@t3;";
+$parser = new PHPSQLParser($sql);
+$p = $parser->parsed;
+$expected = getExpectedValue(dirname(__FILE__), 'variables1.serialized');
+eq_array($p, $expected, 'user variables');
+
+$sql = "SELECT (@aa:=id) AS a, (@aa+3) AS b FROM tbl_name HAVING b=5;";
+$parser = new PHPSQLParser($sql);
+$p = $parser->parsed;
+$expected = getExpectedValue(dirname(__FILE__), 'variables2.serialized');
+eq_array($p, $expected, 'user variables');
