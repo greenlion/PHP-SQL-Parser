014c974f0a41fcc51dae84533f2293bfc4afa27c
Andre Rothe <phosco@gmx.de>
2013-11-09 01:59:30 +0000

    ADD: some new test cases for issue 33 FIX: the parser handles now CREATE TABLE x LIKE y   :-)
    git-svn-id: http://php-sql-parser.googlecode.com/svn/trunk@501 44dcfd11-89b7-3d55-2063-5c365055f77f

--- testsuite/tests/parser/issue33.php
+++ testsuite/tests/parser/issue33.php
@@ -3,6 +3,22 @@ require_once(dirname(__FILE__) . '/../../../php-sql-parser.php');
 require_once(dirname(__FILE__) . '/../../test-more.php');
 
 $parser = new PHPSQLParser();
+$sql = "CREATE TABLE hohoho (LIKE xyz)";
+$parser->parse($sql);
+$p = $parser->parsed;
+print_r($p);
+$expected = getExpectedValue(dirname(__FILE__), 'issue33a.serialized');
+eq_array($p, $expected, 'CREATE TABLE statement with (LIKE)');
+
+$parser = new PHPSQLParser();
+$sql = "CREATE TABLE hohoho LIKE xyz";
+$parser->parse($sql);
+$p = $parser->parsed;
+$expected = getExpectedValue(dirname(__FILE__), 'issue33b.serialized');
+eq_array($p, $expected, 'CREATE TABLE statement with LIKE');
+
+
+$parser = new PHPSQLParser();
 $sql = "CREATE TABLE \"cachetable01\" (
 \"sp_id\" varchar(240) DEFAULT NULL,
 \"ro\" varchar(240) DEFAULT NULL,
