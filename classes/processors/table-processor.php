d2d1d86509c3a8fa22d1993fccbc17dccc854e85
Andre Rothe <phosco@gmx.de>
2013-11-11 01:19:36 +0000

    ADD: some untested code to handle table options wthin CREATE TABLE
    git-svn-id: http://php-sql-parser.googlecode.com/svn/trunk@503 44dcfd11-89b7-3d55-2063-5c365055f77f

--- classes/processors/table-processor.php
+++ classes/processors/table-processor.php
@@ -45,62 +45,265 @@ if (!defined('HAVE_TABLE_PROCESSOR')) {
 
         public function process($tokens) {
 
-            $curr = "TABLENAME";
+            $currCategory = "TABLE_NAME";
+            $result = array();
             $expr = array();
+            $base_expr = "";
 
             foreach ($tokens as $token) {
                 $trim = trim($token);
+                $base_expr .= $token;
 
                 if ($trim === "") {
                     continue;
                 }
 
                 $upper = strtoupper($trim);
+                switch ($upper) {
 
-                if ($upper === 'LIKE') {
-                    # like without parenthesis
-                    $curr = $upper;
-                    continue;
-                }
-
-                if ($curr === "TABLENAME") {
-                    $expr['base_expr'] = $expr['name'] = $trim;
-                    $expr['no_quotes'] = $this->revokeQuotation($trim);
-                    $curr = "";
-                    continue;
-                }
-
-                if ($curr === "LIKE") {
-                    $expr['like'] = array('table' => $trim, 'base_expr' => $trim, 'no_quotes' => $this->revokeQuotation($trim));
-                    $curr = "";
-                    continue;
-                }
-                
-                if ($upper[0] === '(' && substr($upper, -1) === ')') {
-                    $unparsed = $this->splitSQLIntoTokens($this->removeParenthesisFromStart($trim));
-                    $processor = new ColDefProcessor();
-                    $coldef = $processor->process($unparsed);
-
-                    foreach ($coldef as $k => $v) {
-                        if (isset($v['type'])) {
-                            $type = $v['type'];
-                            unset($v['type']);
-                            if ($type === ExpressionType::COLDEF) {
-                                $expr[$type][] = $v;
-                            } else {
-                                if (!isset($expr[$type])) {
-                                    $expr[$type] = array();
-                                }
-                                $expr[$type][] = $v;
-                            }
-                        }
+                case 'UNION':
+                    if ($prevCategory === 'CREATE_DEF') {
+                        $expr[] = array('type' => ExpressionType::RESERVED, 'base_expr' => $trim);
+                        $currCategory = 'UNION';
+                        continue 2;
                     }
+                    break;
 
-                    # TODO:
-                    # after a () we can have multiple table_options and a select_statement
-                    # but only if we don't have set $expr['like'] inside the parenthesis
+                case 'LIKE':
+                # like without parenthesis
+                    if ($prevCategory === 'TABLE_NAME') {
+                        $currCategory = $upper;
+                        continue 2;
+                    }
+                    break;
+
+                case '=':
+                    if ($prevCategory === 'TABLE_OPTION') {
+                        $expr[] = array('type' => ExpressionType::OPERATOR, 'base_expr' => $tim);
+                        continue 2; # don't change the category
+                    }
+                    break;
+
+                case 'CHARACTER':
+                    if ($prevCategory === 'CREATE_DEF') {
+                        $expr[] = array('type' => ExpressionType::RESERVED, 'base_expr' => $trim);
+                        $currCategory = 'TABLE_OPTION';
+                    }
+                    if ($prevCategory === 'TABLE_OPTION') {
+                        # add it to the previous DEFAULT
+                        $expr[] = array('type' => ExpressionType::RESERVED, 'base_expr' => $trim);
+                        continue 2;
+                    }
+                    break;
+
+                case 'SET':
+                    if ($prevCategory === 'TABLE_OPTION') {
+                        # add it to a previous CHARACTER
+                        $expr[] = array('type' => ExpressionType::RESERVED, 'base_expr' => $trim);
+                        $currCategory = 'CHARSET';
+                        continue 2;
+                    }
+                    break;
+
+                case 'COLLATE':
+                    if ($currCategory === 'CHARSET') {
+                        # after character set
+                        $expr[] = array('type' => ExpressionType::RESERVED, 'base_expr' => $trim);
+                        $currCategory = 'COLLATE';
+                        continue 2;
+                    }
+                    break;
+
+                case 'DIRECTORY':
+                    if ($currCategory === 'INDEX_DIRECTORY' || $currCategory === 'DATA_DIRECTORY') {
+                        # after INDEX or DATA
+                        $expr[] = array('type' => ExpressionType::RESERVED, 'base_expr' => $trim);
+                        continue 2;
+                    }
+                    break;
+
+                case 'INDEX':
+                    if ($prevCategory === 'CREATE_DEF') {
+                        $expr[] = array('type' => ExpressionType::RESERVED, 'base_expr' => $trim);
+                        $currCategory = 'INDEX_DIRECTORY';
+                        continue 2;
+                    }
+                    break;
+
+                case 'DATA':
+                    if ($prevCategory === 'CREATE_DEF') {
+                        $expr[] = array('type' => ExpressionType::RESERVED, 'base_expr' => $trim);
+                        $currCategory = 'DATA_DIRECTORY';
+                        continue 2;
+                    }
+                    break;
+
+                case 'INSERT_METHOD':
+                case 'DELAY_KEY_WRITE':
+                case 'ROW_FORMAT':
+                case 'PASSWORD':
+                case 'MAX_ROWS':
+                case 'MIN_ROWS':
+                case 'PACK_KEYS':
+                case 'CHECKSUM':
+                case 'COMMENT':
+                case 'CONNECTION':
+                case 'AUTO_INCREMENT':
+                case 'AVG_ROW_LENGTH':
+                case 'ENGINE':
+                case 'TYPE':
+                    if ($prevCategory === 'CREATE_DEF') {
+                        $expr[] = array('type' => ExpressionType::RESERVED, 'base_expr' => $trim);
+                        $currCategory = 'TABLE_OPTION';
+                    }
+                    break;
+
+                case 'DYNAMIC':
+                case 'FIXED':
+                case 'COMPRESSED':
+                case 'REDUNDANT':
+                case 'COMPACT':
+                case 'NO':
+                case 'FIRST':
+                case 'LAST':
+                case 'DEFAULT':
+                    if ($prevCategory === 'CREATE_DEF') {
+                        # DEFAULT before CHARACTER SET
+                        $expr[] = array('type' => ExpressionType::RESERVED, 'base_expr' => $trim);
+                        $currCategory = 'TABLE_OPTION';
+                    }
+                    if ($prevCategory === 'TABLE_OPTION') {
+                        # all assignments with the keywords
+                        $expr[] = array('type' => ExpressionType::RESERVED, 'base_expr' => $trim);
+                        $result['options'][] = array('type' => ExpressionType::EXPRESSION,
+                                                     'base_expr' => trim($base_expr), 'sub_tree' => $expr);
+                        $expr = array();
+                        $base_expr = '';
+                        $currCategory = 'CREATE_DEF';
+                    }
+                    break;
+
+                default:
+                    switch ($currCategory) {
+
+                    case 'CHARSET':
+                    # charset name
+                        $expr[] = array('type' => ExpressionType::CONSTANT, 'base_expr' => $trim);
+                        $result['options'][] = array('type' => ExpressionType::CHARSET, 'base_expr' => $base_expr,
+                                                     'sub_tree' => $expr);
+                        $base_expr = '';
+                        $expr = array();
+                        $prevCategory = 'CREATE_DEF';
+                        continue 3;
+
+                    case 'COLLATE':
+                    # we have the collate name
+                        $expr[] = array('type' => ExpressionType::CONSTANT, 'base_expr' => $trim);
+                        $last = array_pop($result['options']);
+                        $last['base_expr'] .= $base_expr;
+                        array_merge($last['sub_tree'], $expr);
+                        $result['options'][] = $last;
+
+                        $base_expr = '';
+                        $expr = array();
+                        $prevCategory = 'CREATE_DEF';
+                        continue 3;
+
+                    case 'DATA_DIRECTORY':
+                    # we have the directory name
+                        $expr[] = array('type' => ExpressionType::CONSTANT, 'base_expr' => $trim);
+                        $result['options'][] = array('type' => ExpressionType::DIRECTORY, 'kind' => 'DATA',
+                                                     'base_expr' => $base_expr, 'sub_tree' => $expr);
+                        $base_expr = '';
+                        $expr = array();
+                        $prevCategory = 'CREATE_DEF';
+                        continue 3;
+
+                    case 'INDEX_DIRECTORY':
+                    # we have the directory name
+                        $expr[] = array('type' => ExpressionType::CONSTANT, 'base_expr' => $trim);
+                        $result['options'][] = array('type' => ExpressionType::DIRECTORY, 'kind' => 'INDEX',
+                                                     'base_expr' => $base_expr, 'sub_tree' => $expr);
+                        $base_expr = '';
+                        $expr = array();
+                        $prevCategory = 'CREATE_DEF';
+                        continue 3;
+
+                    case 'TABLE_NAME':
+                        $result['base_expr'] = $result['name'] = $trim;
+                        $result['no_quotes'] = $this->revokeQuotation($trim);
+                        $base_expr = '';
+                        $expr = array();
+                        break;
+
+                    case 'LIKE':
+                        $result['like'] = array('table' => $trim, 'base_expr' => $trim,
+                                                'no_quotes' => $this->revokeQuotation($trim));
+                        $base_expr = '';
+                        $expr = array();
+                        $currCategory = '';
+                        break;
+
+                    case '':
+                    # after table name
+                        if ($upper[0] === '(' && substr($upper, -1) === ')') {
+                            $currCategory = 'CREATE_DEF';
+                            $unparsed = $this->splitSQLIntoTokens($this->removeParenthesisFromStart($trim));
+                            $processor = new ColDefProcessor();
+                            $coldef = $processor->process($unparsed);
+
+                            foreach ($coldef as $k => $v) {
+                                if (isset($v['type'])) {
+                                    $type = $v['type'];
+                                    unset($v['type']);
+                                    if ($type === ExpressionType::COLDEF) {
+                                        $expr[$type][] = $v;
+                                    } else {
+                                        if (!isset($expr[$type])) {
+                                            $expr[$type] = array();
+                                        }
+                                        $expr[$type][] = $v;
+                                    }
+                                }
+                            }
+
+                            # TODO:
+                            # after a () we can have a select_statement
+                            # but only if we don't have set $expr['like'] inside the parenthesis
+                            # stop processing if 'like' is set
+                        }
+                        break;
+
+                    case 'UNION':
+                    # TODO: this token starts and ends with parenthesis
+                    # and contains a list of table names (comma-separated)
+                    # split the token and add the list as subtree
+                        $unparsed = $this->splitSQLIntoTokens($this->removeParenthesisFromStart($trim));
+
+                        $expr[] = array('type' => ExpressionType::BRACKET_EXPRESSION, 'base_expr' => $trim,
+                                        'sub_tree' => '***TODO***');
+                        $result['options'][] = array('type' => ExpressionType::UNION, 'base_expr' => trim($base_expr),
+                                                     'sub_tree' => $expr);
+                        $expr = array();
+                        $base_expr = '';
+                        $currCategory = 'CREATE_DEF';
+                        break;
+
+                    default:
+                    # strings and numeric constants
+                        $expr[] = array('type' => ExpressionType::CONSTANT, 'base_expr' => $trim);
+                        $result['options'][] = array('type' => ExpressionType::EXPRESSION,
+                                                     'base_expr' => trim($base_expr), 'sub_tree' => $expr);
+                        $base_expr = '';
+                        $expr = array();
+                        $currCategory = 'CREATE_DEF';
+                        break;
+                    }
+                    break;
                 }
 
+                $prevCategory = $currCategory;
+                $currCategory = "";
             }
             return $expr;
         }
