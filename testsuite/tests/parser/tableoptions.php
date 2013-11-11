507f30f3123f18b555f4ad044653c09c8bf3397a
Andre Rothe <phosco@gmx.de>
2013-11-11 08:18:02 +0000

    ADD: a test for table options
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@508 44dcfd11-89b7-3d55-2063-5c365055f77f

---
+++ testsuite/tests/parser/tableoptions.php
@@ -0,0 +1,11 @@
+<?php
+require_once(dirname(__FILE__) . '/../../../php-sql-parser.php');
+require_once(dirname(__FILE__) . '/../../test-more.php');
+
+$parser = new PHPSQLParser();
+$sql = "CREATE TABLE hohoho () AUTO_INCREMENT = 1 DEFAULT CHARACTER SET _utf8 PASSWORD 'test123'";
+$parser->parse($sql);
+$p = $parser->parsed;
+print_r($p);
+$expected = getExpectedValue(dirname(__FILE__), 'tableoptions.serialized');
+eq_array($p, $expected, 'CREATE TABLE statement with table options');
