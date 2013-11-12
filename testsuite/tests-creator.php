29e736337e4be8a3262727e2c9c42ec92900e166
Andre Rothe <phosco@gmx.de>
2013-11-12 08:45:37 +0000

    ADD: a test case for issue 100, there are aliases of constants...
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@518 44dcfd11-89b7-3d55-2063-5c365055f77f

--- testsuite/tests-creator.php
+++ testsuite/tests-creator.php
@@ -24,6 +24,7 @@ require_once(dirname(__FILE__) . '/tests/creator/issue88.php');
 require_once(dirname(__FILE__) . '/tests/creator/issue92.php');
 require_once(dirname(__FILE__) . '/tests/creator/issue94.php');
 require_once(dirname(__FILE__) . '/tests/creator/issue98.php');
+require_once(dirname(__FILE__) . '/tests/creator/issue100.php');
 require_once(dirname(__FILE__) . '/tests/creator/join.php');
 require_once(dirname(__FILE__) . '/tests/creator/left.php');
 require_once(dirname(__FILE__) . '/tests/creator/magnus.php');
