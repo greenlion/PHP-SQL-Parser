55c197010f102ed07015eec6cf7fb3cd07492845
Andre Rothe <phosco@gmx.de>
2013-11-14 23:56:55 +0000

    ADD: some code to handle the create-definions within CREATE TABLE.
    git-svn-id: http://php-sql-parser.googlecode.com/svn/trunk@522 44dcfd11-89b7-3d55-2063-5c365055f77f

--- classes/processors/col-def-processor.php
+++ classes/processors/col-def-processor.php
@@ -43,7 +43,122 @@ if (!defined('HAVE_COL_DEF_PROCESSOR')) {
     class ColDefProcessor extends AbstractProcessor {
 
         public function process($tokens) {
-            return array();
+
+            $createDef = array();
+            $prevCategory = "";
+            $currCategory = "";
+            $expr = array();
+            $result = array();
+
+            foreach ($tokens as $k => $token) {
+
+                $trim = trim($token);
+                if ($trim === "") {
+                    $createDef[] = $token;
+                    continue;
+                }
+
+                $upper = strtoupper($trim);
+
+                switch ($upper) {
+
+                case 'CONSTRAINT':
+                case 'LIKE':
+                    $currCategory = $prevCategory = $upper;
+                    continue 2;
+
+                case 'FOREIGN':
+                case 'PRIMARY':
+                case 'UNIQUE':
+                    $currCategory = $upper;
+                    if ($prevCategory === "" || $prevCategory === "CONSTRAINT") {
+                        # next one is KEY
+                        continue 2;
+                    }
+                    break;
+
+                case 'KEY':
+                # the next one is an index name
+                    if ($currCategory === 'PRIMARY' && $currCategory === 'FOREIGN') {
+                        continue 2;
+                    }
+                    $currCategory = $upper;
+                    continue 2;
+
+                case 'CHECK':
+                case 'INDEX':
+                    if ($currCategory === 'UNIQUE') {
+                        # index after unique
+                        continue 2;
+                    }
+                    $currCategory = $upper;
+                    continue 2;
+
+                case 'FULLTEXT':
+                case 'SPATIAL':
+                    $currCategory = $prevCategory = $upper;
+                    continue 2;
+
+                case 'WITH':
+                    break;
+
+                case 'PARSER':
+                    break;
+                    
+                case ',':
+                    # this starts the next definition
+                    break;
+
+                default:
+                    switch ($currCategory) {
+
+                    case 'LIKE':
+                    # this is the tablename after LIKE
+                        $result['like'] = array('table' => $trim, 'base_expr' => $trim,
+                                                'no_quotes' => $this->revokeQuotation($trim));
+
+                        break;
+
+                    case 'PRIMARY':
+                    # this could be the index_type BTREE or HASH
+                    # or if we have parenthesis, then we have the list of index_columns
+                        break;
+
+                    case 'FOREIGN':
+                    # this could be the index_name
+                    # or if we have parenthesis, then we have the list of index_columns
+                        break;
+
+                    case 'CONSTRAINT':
+                    # this is the constraint symbol
+                        break;
+
+                    case 'INDEX':
+                    # this is the index name (after SPATIAL, FULLTEXT, INDEX)
+                        break;
+
+                    case 'CHECK':
+                    # if we have parenthesis, then we have the check expression
+                        break;
+
+                    case 'KEY':
+                    # this could be the index_name or index_type (BTREE or HASH)
+                        break;
+
+                    case 'UNIQUE':
+                    # this could be the index_name or index_type (BTREE or HASH)
+                        break;
+
+                    default:
+                        break;
+                    }
+                    break;
+                }
+                $prevCategory = currCategory;
+                $currCategory = "";
+
+            }
+            return $result;
         }
     }
     define('HAVE_COL_DEF_PROCESSOR', 1);
