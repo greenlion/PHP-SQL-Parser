9664272356a06933a5dc164ec53aca4e365d2923
Andre Rothe <phosco@gmx.de>
2013-12-08 00:59:04 +0000

    CHG: all test cases for issue 33 have been activated
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@623 44dcfd11-89b7-3d55-2063-5c365055f77f

--- testsuite/tests/parser/issue33.php
+++ testsuite/tests/parser/issue33.php
@@ -9,12 +9,14 @@ $p = $parser->parsed;
 $expected = getExpectedValue(dirname(__FILE__), 'issue33a.serialized');
 eq_array($p, $expected, 'CREATE TABLE statement with (LIKE)');
 
+
 $parser = new PHPSQLParser();
 $sql = "CREATE TABLE hohoho LIKE xyz";
 $parser->parse($sql);
 $p = $parser->parsed;
 $expected = getExpectedValue(dirname(__FILE__), 'issue33b.serialized');
 eq_array($p, $expected, 'CREATE TABLE statement with LIKE');
+
 
 $parser = new PHPSQLParser();
 $sql = "CREATE TABLE hohoho (a varchar(1000) NOT NULL, CONSTRAINT hohoho_pk PRIMARY KEY (a), CHECK(a > 5))";
@@ -23,6 +25,7 @@ $p = $parser->parsed;
 $expected = getExpectedValue(dirname(__FILE__), 'issue33c.serialized');
 eq_array($p, $expected, 'CREATE TABLE statement with named primary key and check');
 
+
 $parser = new PHPSQLParser();
 $sql = "CREATE TABLE hohoho (a varchar(1000), CONSTRAINT PRIMARY KEY (a), CHECK(a > 5))";
 $parser->parse($sql);
@@ -30,12 +33,14 @@ $p = $parser->parsed;
 $expected = getExpectedValue(dirname(__FILE__), 'issue33d.serialized');
 eq_array($p, $expected, 'CREATE TABLE statement with primary key and check');
 
+
 $parser = new PHPSQLParser();
 $sql = "CREATE TABLE hohoho (a varchar(1000), PRIMARY KEY USING btree (a), CHECK(a > 5))";
 $parser->parse($sql);
 $p = $parser->parsed;
 $expected = getExpectedValue(dirname(__FILE__), 'issue33e.serialized');
 eq_array($p, $expected, 'CREATE TABLE statement with primary key and check');
+
 
 $parser = new PHPSQLParser();
 $sql = "CREATE TABLE \"cachetable01\" (
@@ -49,14 +54,14 @@ $p = $parser->parsed;
 $expected = getExpectedValue(dirname(__FILE__), 'issue33f.serialized');
 eq_array($p, $expected, 'CREATE TABLE statement');
 
-// FIXME: the keywords "with parser" have been loose
+
 $parser = new PHPSQLParser();
 $sql = "CREATE TABLE hohoho (a varchar(1000), PRIMARY KEY USING btree (a(5) ASC) key_block_size 4 with parser haha, CHECK(a > 5))";
 $parser->parse($sql);
 $p = $parser->parsed;
-print_r($p);
 $expected = getExpectedValue(dirname(__FILE__), 'issue33g.serialized');
 eq_array($p, $expected, 'CREATE TABLE statement with primary key with index options and check');
+
 
 $parser = new PHPSQLParser();
 $sql = "CREATE TABLE hohoho (a varchar(1000)) ENGINE=xyz,COMMENT='haha' DEFAULT COLLATE = latin1_german2_ci";
@@ -65,12 +70,14 @@ $p = $parser->parsed;
 $expected = getExpectedValue(dirname(__FILE__), 'issue33h.serialized');
 eq_array($p, $expected, 'CREATE TABLE statement with table options separated by different characters');
 
+
 $parser = new PHPSQLParser();
 $sql = "CREATE TABLE hohoho (a varchar(1000), b integer, FOREIGN KEY haha (b) references xyz (id) match full on delete cascade) ";
 $parser->parse($sql);
 $p = $parser->parsed;
 $expected = getExpectedValue(dirname(__FILE__), 'issue33i.serialized');
 eq_array($p, $expected, 'CREATE TABLE statement with foreign key references');
+
 
 $parser = new PHPSQLParser();
 $sql = "CREATE TABLE turma(id text NOT NULL ,
@@ -83,3 +90,10 @@ $p = $parser->parsed;
 $expected = getExpectedValue(dirname(__FILE__), 'issue33j.serialized');
 eq_array($p, $expected, 'simple CREATE TABLE statement');
 
+
+$parser = new PHPSQLParser();
+$sql = "CREATE TABLE hohoho (a varchar(1000), PRIMARY KEY (a(5) ASC) key_block_size 4 using btree with parser haha, CHECK(a > 5))";
+$parser->parse($sql);
+$p = $parser->parsed;
+$expected = getExpectedValue(dirname(__FILE__), 'issue33k.serialized');
+eq_array($p, $expected, 'CREATE TABLE statement with primary key and multiple index options and check');
