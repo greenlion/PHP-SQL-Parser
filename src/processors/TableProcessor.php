c0df1e70c5c7ec9005f38a9e721abd5011e376bb
Andre Rothe <phosco@gmx.de>
2013-12-10 11:40:25 +0000

    CHG: the class CreateDefProcessor has been renamed into CreateDefinitionProcessor, so we have to change all references.
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@697 44dcfd11-89b7-3d55-2063-5c365055f77f

--- src/processors/TableProcessor.php
+++ src/processors/TableProcessor.php
@@ -303,7 +303,7 @@ class TableProcessor extends AbstractProcessor {
                 # after table name
                     if ($prevCategory === 'TABLE_NAME' && $upper[0] === '(' && substr($upper, -1) === ')') {
                         $unparsed = $this->splitSQLIntoTokens($this->removeParenthesisFromStart($trim));
-                        $processor = new CreateDefProcessor();
+                        $processor = new CreateDefinitionProcessor();
                         $coldef = $processor->process($unparsed);
                         $result['create-def'] = array('expr_type' => ExpressionType::BRACKET_EXPRESSION,
                                                       'base_expr' => $base_expr, 'sub_tree' => $coldef['create-def']);
