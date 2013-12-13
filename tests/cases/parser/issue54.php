69a38a9ba75436d9929afac4bd4c86aa45fd5ad2
Andre Rothe <phosco@gmx.de>
2013-12-13 13:24:09 +0000

    CHG: apply the new code format rules
    git-svn-id: http://php-sql-parser.googlecode.com/svn/trunk@741 44dcfd11-89b7-3d55-2063-5c365055f77f

--- tests/cases/parser/issue54.php
+++ tests/cases/parser/issue54.php
@@ -1,6 +1,6 @@
 <?php
-require_once(dirname(__FILE__) . "/../../../src/PHPSQLParser.php");
-require_once(dirname(__FILE__) . "/../../test-more.php");
+require_once dirname(__FILE__) . "/../../../src/PHPSQLParser.php";
+require_once dirname(__FILE__) . "/../../test-more.php";
 
 $errorNumber = 0;
 
