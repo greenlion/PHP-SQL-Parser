31ee8e28424d60c7fa4b03c773a8090ed60b2a31
Andre Rothe <phosco@gmx.de>
2013-12-16 09:54:05 +0000

    CHG: apply the new code format rules, the file has no format errors
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@757 44dcfd11-89b7-3d55-2063-5c365055f77f

--- src/PHPSQLParser.php
+++ src/PHPSQLParser.php
@@ -5,30 +5,38 @@
  *
  * A pure PHP SQL (non validating) parser w/ focus on MySQL dialect of SQL
  *
- * Copyright (c) 2010-2012, Justin Swanhart
- * with contributions by André Rothe <arothe@phosco.info, phosco@gmx.de>
+ * PHP version 5
  *
+ * LICENSE:
+ * Copyright (c) 2010-2014 Justin Swanhart and André Rothe
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
  *
- *   * Redistributions of source code must retain the above copyright notice,
- *     this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above copyright notice,
- *     this list of conditions and the following disclaimer in the documentation
- *     and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
- * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
- * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
- * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
- * DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * @author    André Rothe <andre.rothe@phosco.info>
+ * @copyright 2010-2014 Justin Swanhart and André Rothe
+ * @license   http://www.debian.org/misc/bsd.license  BSD License (3 Clause)
+ * @version   SVN: $Id$
  */
 
 require_once dirname(__FILE__) . '/positions/PositionCalculator.php';
@@ -37,19 +45,40 @@ require_once dirname(__FILE__) . '/processors/DefaultProcessor.php';
 /**
  * This class implements the parser functionality.
  *
- * @author greenlion@gmail.com
- * @author arothe@phosco.info
+ * @author  Justin Swanhart <greenlion@gmail.com>
+ * @author  André Rothe <arothe@phosco.info>
+ * @license http://www.debian.org/misc/bsd.license  BSD License (3 Clause)
  */
 class PHPSQLParser {
 
     public $parsed;
 
+    /**
+     * Constructor. It simply calls the parse() function. 
+     * Use the public variable $parsed to get the output.
+     * 
+     * @param String  $sql           The SQL statement.
+     * @param boolean $calcPositions True, if the output should contain [position], false otherwise.
+     */
     public function __construct($sql = false, $calcPositions = false) {
         if ($sql) {
             $this->parse($sql, $calcPositions);
         }
     }
 
+    /**
+     * It parses the given SQL statement and generates a detailled 
+     * output array for every part of the statement. The method can 
+     * also generate [position] fields within the output, which hold 
+     * the character position for every statement part. The calculation 
+     * of the positions needs some time, if you don't need positions in
+     * your application, set the parameter to false.
+     * 
+     * @param String  $sql           The SQL statement.
+     * @param boolean $calcPositions True, if the output should contain [position], false otherwise.
+     * 
+     * @return array An associative array with all meta information about the SQL statement.
+     */
     public function parse($sql, $calcPositions = false) {
 
         $processor = new DefaultProcessor();
