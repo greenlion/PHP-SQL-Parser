6e0609cd44e561a09b452d1fa453b01476a3475d
Andre Rothe <phosco@gmx.de>
2013-12-17 16:32:45 +0000

    CHG: some require_once have needed corrections
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@806 44dcfd11-89b7-3d55-2063-5c365055f77f

--- src/builders/SetBuilder.php
+++ src/builders/SetBuilder.php
@@ -39,12 +39,8 @@
  * 
  */
 
-require_once dirname(__FILE__) . '/../utils/ExpressionType.php';
 require_once dirname(__FILE__) . '/../exceptions/UnableToCreateSQLException.php';
-require_once dirname(__FILE__) . '/ColumnReferenceBuilder.php';
-require_once dirname(__FILE__) . '/ConstantBuilder.php';
-require_once dirname(__FILE__) . '/OperatorBuilder.php';
-require_once dirname(__FILE__) . '/FunctionBuilder.php';
+require_once dirname(__FILE__) . '/SetExpressionBuilder.php';
 
 /**
  * This class implements the builder for the SET part of INSERT statement. 
