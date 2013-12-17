3b4613eab0d52438acb76d9ee2c33dca509a8b67
Andre Rothe <phosco@gmx.de>
2013-12-17 21:02:06 +0000

    ADD: a new test case for issue 105
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@826 44dcfd11-89b7-3d55-2063-5c365055f77f

---
+++ tests/cases/creator/issue105.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once dirname(__FILE__) . '/../../../src/PHPSQLParser.php';
+require_once dirname(__FILE__) . '/../../../src/PHPSQLCreator.php';
+require_once dirname(__FILE__) . '/../../test-more.php';
+
+$sql = "SELECT users0.user_name AS 'CIS UserName'
+	,calls.description AS 'Description'
+	,contacts2.first_name AS 'Contacts First Name'
+	,contacts2.last_name AS 'Contacts Last Name'
+	,calls_cstm.date_logged_c AS 'Date'
+	,calls_cstm.contact_type_c AS 'Contact Type'
+	,dbo.fn_GetAccountName(calls.parent_id) AS 'Account Name'
+FROM calls
+LEFT JOIN calls_cstm ON calls.id = calls_cstm.id_c
+LEFT JOIN users users0 ON calls.assigned_user_id = users0.id
+LEFT JOIN contacts contacts2 ON calls.contact_id = contacts2.id
+WHERE calls.deleted = 0
+	AND (
+		DATEADD(SECOND, 0, calls_cstm.date_logged_c) BETWEEN '2013-01-01'
+			AND '2013-12-31'
+		)
+ORDER BY dbo.fn_GetAccountName(calls.parent_id) ASC LIMIT 0
+	,15";
+$parser = new PHPSQLParser($sql);
+$creator = new PHPSQLCreator($parser->parsed);
+$created = $creator->created;
+print_r($created);
+$expected = getExpectedValue(dirname(__FILE__), 'issue105.sql', false);
+ok($created === $expected, 'function within order-by');
+
+?>
