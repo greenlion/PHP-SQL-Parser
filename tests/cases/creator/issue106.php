8b6dfda46fef5e23953782cc696982262abd38b3
Andre Rothe <phosco@gmx.de>
2013-12-18 07:56:08 +0000

    ADD: a new test case for issue 106 REM: the output statement within issue105.php
    git-svn-id: http://php-sql-parser.googlecode.com/svn/trunk@828 44dcfd11-89b7-3d55-2063-5c365055f77f

---
+++ tests/cases/creator/issue106.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once dirname(__FILE__) . '/../../../src/PHPSQLParser.php';
+require_once dirname(__FILE__) . '/../../../src/PHPSQLCreator.php';
+require_once dirname(__FILE__) . '/../../test-more.php';
+
+$sql = "SELECT dbo.fn_GetDayOfWeekMonIs0(DATEADD(SECOND, -21600, calls_cstm.date_logged_c)) AS 'Date'
+FROM calls
+LEFT JOIN calls_cstm ON calls.id = calls_cstm.id_c
+LEFT JOIN users users0 ON calls.assigned_user_id = users0.id
+LEFT JOIN contacts contacts2 ON calls.contact_id = contacts2.id
+WHERE calls.deleted = 0
+	AND (
+		(dbo.fn_GetDayOfWeekMonIs0(DATEADD(SECOND, 0, calls_cstm.date_logged_c)) IN ('4'))
+		AND DATENAME(YEAR, DATEADD(SECOND, 0, calls_cstm.date_logged_c)) = '2013'
+		AND (DATEPART(MONTH, DATEADD(SECOND, 0, calls_cstm.date_logged_c)) IN ('10'))
+		)
+GROUP BY dbo.fn_GetDayOfWeekMonIs0(DATEADD(SECOND, -21600, calls_cstm.date_logged_c))
+ORDER BY dbo.fn_GetDayOfWeekMonIs0(DATEADD(SECOND, -21600, calls_cstm.date_logged_c)) ASC";
+$parser = new PHPSQLParser($sql);
+$creator = new PHPSQLCreator($parser->parsed);
+$created = $creator->created;
+$expected = getExpectedValue(dirname(__FILE__), 'issue106.sql', false);
+ok($created === $expected, 'function within group-by');
+
+?>
