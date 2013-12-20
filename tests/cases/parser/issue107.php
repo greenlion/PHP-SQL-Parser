0122a809f105e209b401fba2b3238d1cbd9155f4
Andre Rothe <phosco@gmx.de>
2013-12-20 05:41:20 +0000

    ADD: a test case for issue 107.
    git-svn-id: http://php-sql-parser.googlecode.com/svn/trunk@837 44dcfd11-89b7-3d55-2063-5c365055f77f

---
+++ tests/cases/parser/issue107.php
@@ -0,0 +1,20 @@
+<?php
+require_once(dirname(__FILE__) . "/../../../src/PHPSQLParser.php");
+require_once(dirname(__FILE__) . "/../../test-more.php");
+
+try {
+    $sql = "CREATE TABLE IF NOT EXISTS `engine4_urdemo_causebug` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `extra` int(11)  NOT NULL DEFAULT 56,
+  PRIMARY KEY (`id`),
+  INDEX client_idx (id)
+) ENGINE=InnoDB;";
+    $parser = new PHPSQLParser($sql);
+    $p = $parser->parsed;
+} catch (Exception $e) {
+    $p = array();
+}
+ok($p['TABLE']['create-def']['sub_tree'][1]['sub_tree'][1]['sub_tree'][5]['expr_type'] === ExpressionType::DEF_VALUE,
+        'column definition with DEFAULT value');
+
+?>
