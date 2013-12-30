5c91dd0e59520f89a04587e65ddf573f30dfe668
Andre Rothe <phosco@gmx.de>
2013-12-30 10:59:13 +0000

    CHG: the comment characters (according cody style rules).
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@860 44dcfd11-89b7-3d55-2063-5c365055f77f

--- tests/cases/parser/issue67.php
+++ tests/cases/parser/issue67.php
@@ -2,17 +2,18 @@
 require_once(dirname(__FILE__) . "/../../../src/PHPSQLParser.php");
 require_once(dirname(__FILE__) . "/../../test-more.php");
 
-# the SET statement doesn't work completely, SESSION is not a colref!
+// TODO: the SET statement doesn't work completely, SESSION is not a colref!
 $sql = "SET SESSION group_concat_max_len = @@max_allowed_packet";
 $parser = new PHPSQLParser($sql, true);
 $p = $parser->parsed;
 $expected = getExpectedValue(dirname(__FILE__), 'issue67a.serialized');
 eq_array($p, $expected, '@ character after operator should not fail.');
 
-# this is ok
+// this is ok
 $sql = "SET @a = 1";
 $parser = new PHPSQLParser($sql, true);
 $p = $parser->parsed;
+echo serialize($p);
 $expected = getExpectedValue(dirname(__FILE__), 'issue67b.serialized');
 eq_array($p, $expected, 'user defined variables should not fail');
 
