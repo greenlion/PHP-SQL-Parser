9c53f48a28216854504dab841f40c8b01e31b138
Andre Rothe <phosco@gmx.de>
2013-12-30 11:09:50 +0000

    ADD: a TODO
    git-svn-id: http://php-sql-parser.googlecode.com/svn/trunk@862 44dcfd11-89b7-3d55-2063-5c365055f77f

--- tests/cases/parser/issue68.php
+++ tests/cases/parser/issue68.php
@@ -2,6 +2,7 @@
 require_once(dirname(__FILE__) . "/../../../src/PHPSQLParser.php");
 require_once(dirname(__FILE__) . "/../../test-more.php");
 
+// TODO: there is an error within no_quotes
 $sql = "select a.`admin_id` FROM admins a WHERE a.admin_username=? AND a.admin_password=?";
 $parser = new PHPSQLParser($sql, true);
 $p = $parser->parsed;
