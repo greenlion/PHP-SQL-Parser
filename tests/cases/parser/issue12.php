ca0302c656c911e8cb27f63c740a5848103cfad7
Andre Rothe <phosco@gmx.de>
2013-12-30 09:52:29 +0000

    CHG: the output has been removed again.
    git-svn-id: http://php-sql-parser.googlecode.com/svn/trunk@848 44dcfd11-89b7-3d55-2063-5c365055f77f

--- tests/cases/parser/issue12.php
+++ tests/cases/parser/issue12.php
@@ -6,8 +6,6 @@ $parser = new PHPSQLParser();
 
 $sql = "SELECT SQL_CALC_FOUND_ROWS SmTable.*, MATCH (SmTable.fulltextsearch_keyword) AGAINST ('google googles' WITH QUERY EXPANSION) AS keyword_score FROM SmTable WHERE SmTable.status = 'A' AND (SmTable.country_id = 1 AND SmTable.state_id = 10) AND MATCH (SmTable.fulltextsearch_keyword) AGAINST ('google googles') ORDER BY SmTable.level DESC, keyword_score DESC LIMIT 0,10";
 $p = $parser->parse($sql, true);
-print_r($p);
-echo serialize($p);
 $expected = getExpectedValue(dirname(__FILE__), 'issue12.serialized');
 eq_array($p, $expected, 'issue 12');
 
