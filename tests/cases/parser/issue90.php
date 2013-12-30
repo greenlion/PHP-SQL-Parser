1fed1c6c7aec4843f6090d74288f4718a37d3eaa
Andre Rothe <phosco@gmx.de>
2013-12-30 19:51:16 +0000

    ADD: a TODO, the ON DUPLICATE KEY UPDATE are not correctly parsed.
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@879 44dcfd11-89b7-3d55-2063-5c365055f77f

--- tests/cases/parser/issue90.php
+++ tests/cases/parser/issue90.php
@@ -3,9 +3,11 @@ require_once(dirname(__FILE__) . "/../../../src/PHPSQLParser.php");
 require_once(dirname(__FILE__) . "/../../test-more.php");
 
 
+// TODO: the keywords ON DUPLICATE KEY UPDATE are not parsed
 $sql = 'INSERT INTO table (a,b,c) VALUES (1,2,3) ON DUPLICATE KEY UPDATE id=LAST_INSERT_ID(id), c=3;';
 $parser = new PHPSQLParser();
 $p = $parser->parse($sql);
+print_r($p);
 $expected = getExpectedValue(dirname(__FILE__), 'issue90.serialized');
 eq_array($p, $expected, 'on duplicate key problem');
 
