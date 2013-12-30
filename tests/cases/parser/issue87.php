4db1858c764b8d52c3946ad581d583015735b3be
Andre Rothe <phosco@gmx.de>
2013-12-30 15:05:16 +0000

    ADD: a TODO for the RENAME statements
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@878 44dcfd11-89b7-3d55-2063-5c365055f77f

--- tests/cases/parser/issue87.php
+++ tests/cases/parser/issue87.php
@@ -2,6 +2,10 @@
 require_once(dirname(__FILE__) . "/../../../src/PHPSQLParser.php");
 require_once(dirname(__FILE__) . "/../../test-more.php");
 
+// TODO: we should store the object type under the RENAME field
+// because we can also rename users
+// after that we should use sub_tree to split all relevant keywords for the
+// position calculator
 $parser = new PHPSQLParser();
 $sql = "RENAME TABLE a TO b";
 $parser->parse($sql, true);
