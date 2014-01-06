420af233370679c40bac1fb18e4cae7e17d3dc33
Andre Rothe <phosco@gmx.de>
2014-01-06 09:46:48 +0000

    FIX: issue 110
    git-svn-id: http://php-sql-parser.googlecode.com/svn/trunk@901 44dcfd11-89b7-3d55-2063-5c365055f77f

--- tests/cases/creator/issue110.php
+++ tests/cases/creator/issue110.php
@@ -4,12 +4,10 @@ require_once dirname(__FILE__) . '/../../../src/PHPSQLParser.php';
 require_once dirname(__FILE__) . '/../../../src/PHPSQLCreator.php';
 require_once dirname(__FILE__) . '/../../test-more.php';
 
-# TODO: there is a comma behind the DISTINCT keyword
 $sql = 'SELECT DISTINCT a FROM b';
 $parser = new PHPSQLParser($sql);
 $creator = new PHPSQLCreator($parser->parsed);
 $created = $creator->created;
-print_r($created);
 $expected = getExpectedValue(dirname(__FILE__), 'issue110.sql', false);
 ok($created === $expected, 'simple select with distinct option');
 
