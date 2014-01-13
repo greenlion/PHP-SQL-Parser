3940a144a6a40a751a9a87b2ae1c03a188a0f2a7
Andre Rothe <phosco@gmx.de>
2014-01-13 10:03:14 +0000

    ADD: new test cases for issue 84
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@1003 44dcfd11-89b7-3d55-2063-5c365055f77f

---
+++ tests/cases/parser/issue84.php
@@ -0,0 +1,27 @@
+<?php
+require_once(dirname(__FILE__) . "/../../../src/PHPSQLParser.php");
+require_once(dirname(__FILE__) . "/../../test-more.php");
+
+$parser = new PHPSQLParser();
+$sql = "INSERT INTO newTablename SELECT field1, field2, field3 FROM oldTablename where field1 > 100";
+$parser->parse($sql, true);
+$p = $parser->parsed;
+$expected = getExpectedValue(dirname(__FILE__), 'issue84a.serialized');
+eq_array($p, $expected, 'INSERT ... SELECT .. FROM ... WHERE');
+
+
+$parser = new PHPSQLParser();
+$sql = "INSERT INTO newTablename (SELECT field1, field2, field3 FROM oldTablename where field1 > 100)";
+$parser->parse($sql, true);
+$p = $parser->parsed;
+$expected = getExpectedValue(dirname(__FILE__), 'issue84b.serialized');
+eq_array($p, $expected, 'INSERT ... (SELECT .. FROM ... WHERE)');
+
+
+$parser = new PHPSQLParser();
+$sql = "INSERT INTO newTablename (field1, field2, field3) VALUES (1, 2, 3)";
+$parser->parse($sql, true);
+$p = $parser->parsed;
+$expected = getExpectedValue(dirname(__FILE__), 'issue84c.serialized');
+eq_array($p, $expected, 'INSERT ... (cols) VALUES (values)');
+?>
