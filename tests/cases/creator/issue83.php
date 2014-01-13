7ede58ba3597024194e82be35c75e1795d1c5b91
Andre Rothe <phosco@gmx.de>
2014-01-13 12:00:30 +0000

    ADD: new test cases for issue 83
    git-svn-id: http://php-sql-parser.googlecode.com/svn/trunk@1015 44dcfd11-89b7-3d55-2063-5c365055f77f

--- tests/cases/creator/issue83.php
+++ tests/cases/creator/issue83.php
@@ -4,12 +4,28 @@ require_once dirname(__FILE__) . '/../../../src/PHPSQLParser.php';
 require_once dirname(__FILE__) . '/../../../src/PHPSQLCreator.php';
 require_once dirname(__FILE__) . '/../../test-more.php';
 
-#TODO: not solved, the parser doesn't handle subselects here
-$sql = "INSERT INTO newTablename (SELECT * FROM oldTablename);";
-$parser = new PHPSQLParser($sql, true);
+$parser = new PHPSQLParser();
+$sql = "INSERT INTO newTablename SELECT field1, field2, field3 FROM oldTablename where field1 > 100";
+$parser->parse($sql, true);
 $creator = new PHPSQLCreator($parser->parsed);
 $created = $creator->created;
-$expected = getExpectedValue(dirname(__FILE__), 'issue83.sql', false);
-ok($created === $expected, 'Subselect instead of value list.');
+$expected = getExpectedValue(dirname(__FILE__), 'issue83a.sql', false);
+ok($created === $expected, 'INSERT ... SELECT .. FROM ... WHERE');
+
+$parser = new PHPSQLParser();
+$sql = "INSERT INTO newTablename (SELECT field1, field2, field3 FROM oldTablename where field1 > 100)";
+$parser->parse($sql, true);
+$creator = new PHPSQLCreator($parser->parsed);
+$created = $creator->created;
+$expected = getExpectedValue(dirname(__FILE__), 'issue83b.sql', false);
+ok($created === $expected, 'INSERT ... (SELECT .. FROM ... WHERE)');
+
+$parser = new PHPSQLParser();
+$sql = "INSERT INTO newTablename (field1, field2, field3) VALUES (1, 2, 3)";
+$parser->parse($sql, true);
+$creator = new PHPSQLCreator($parser->parsed);
+$created = $creator->created;
+$expected = getExpectedValue(dirname(__FILE__), 'issue83c.sql', false);
+ok($created === $expected, 'INSERT ... (cols) VALUES (values)');
 
 ?>
