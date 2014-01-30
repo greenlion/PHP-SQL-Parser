f4099916976efd90f171f0155a917e3a30922bc9
Andre Rothe <phosco@gmx.de>
2014-01-30 11:56:20 +0000

    ADD: a new test case for issue 117
    git-svn-id: http://php-sql-parser.googlecode.com/svn/trunk@1056 44dcfd11-89b7-3d55-2063-5c365055f77f

---
+++ tests/cases/parser/issue117.php
@@ -0,0 +1,11 @@
+<?php
+require_once(dirname(__FILE__) . "/../../../src/PHPSQLParser.php");
+require_once(dirname(__FILE__) . "/../../test-more.php");
+
+$sql = "(((SELECT x FROM table)) ORDER BY x)";
+$parser = new PHPSQLParser($sql, true);
+$p = $parser->parsed;
+$expected = getExpectedValue(dirname(__FILE__), 'issue117.serialized');
+eq_array($p, $expected, 'parentheses on the first position of statement');
+
+?>
