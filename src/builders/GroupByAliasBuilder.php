c2410f546cd4e74a1033d9d99a545be291e6e61a
Andre Rothe <phosco@gmx.de>
2014-01-30 12:25:16 +0000

    CHG: set some svn properties
    git-svn-id: http://php-sql-parser.googlecode.com/svn/trunk@1061 44dcfd11-89b7-3d55-2063-5c365055f77f

--- src/builders/GroupByAliasBuilder.php
+++ src/builders/GroupByAliasBuilder.php
@@ -35,7 +35,7 @@
  * @author    André Rothe <andre.rothe@phosco.info>
  * @copyright 2010-2014 Justin Swanhart and André Rothe
  * @license   http://www.debian.org/misc/bsd.license  BSD License (3 Clause)
- * @version   SVN: $Id: GroupByAliasBuilder.php 1005 2014-01-13 11:12:29Z phosco@gmx.de $
+ * @version   SVN: $Id$
  * 
  */
 
