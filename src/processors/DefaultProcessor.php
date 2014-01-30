68cbb0b59f2fe5f7d05039b4e339db649f03508e
Andre Rothe <phosco@gmx.de>
2014-01-30 14:31:59 +0000

    CHG: some more protected functions to change the behaviour within subclasses
    git-svn-id: http://php-sql-parser.googlecode.com/svn/trunk@1069 44dcfd11-89b7-3d55-2063-5c365055f77f

--- src/processors/DefaultProcessor.php
+++ src/processors/DefaultProcessor.php
@@ -52,18 +52,22 @@ require_once dirname(__FILE__) . '/SQLProcessor.php';
  */
 class DefaultProcessor extends AbstractProcessor {
 
+    protected function isUnion($tokens) {
+        return UnionProcessor::isUnion($tokens);
+    }
+
     protected function processUnion($tokens) {
         // this is the highest level lexical analysis. This is the part of the
         // code which finds UNION and UNION ALL query parts
         $processor = new UnionProcessor();
         return $processor->process($tokens);
     }
-    
+
     protected function processSQL($tokens) {
         $processor = new SQLProcessor();
         return $processor->process($tokens);
     }
-    
+
     public function process($sql) {
 
         $inputArray = $this->splitSQLIntoTokens($sql);
@@ -71,7 +75,7 @@ class DefaultProcessor extends AbstractProcessor {
 
         // If there was no UNION or UNION ALL in the query, then the query is
         // stored at $queries[0].
-        if (!$processor->isUnion($queries)) {
+        if (!$this->isUnion($queries)) {
             $queries = $this->processSQL($queries[0]);
         }
 
