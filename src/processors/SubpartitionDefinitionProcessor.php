0cc4979fe5cea8811784157069d2da6c612bd5bf
Andre Rothe <phosco@gmx.de>
2014-01-30 12:24:58 +0000

    CHG: set some svn properties
    git-svn-id: http://php-sql-parser.googlecode.com/svn/trunk@1060 44dcfd11-89b7-3d55-2063-5c365055f77f

--- src/processors/SubpartitionDefinitionProcessor.php
+++ src/processors/SubpartitionDefinitionProcessor.php
@@ -36,7 +36,7 @@
  * @author    André Rothe <andre.rothe@phosco.info>
  * @copyright 2010-2014 Justin Swanhart and André Rothe
  * @license   http://www.debian.org/misc/bsd.license  BSD License (3 Clause)
- * @version   SVN: $Id: CreateDefinitionProcessor.php 944 2014-01-08 20:03:21Z phosco@gmx.de $
+ * @version   SVN: $Id$
  *
  */
 
