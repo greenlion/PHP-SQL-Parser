cae66133d0c255785b52c012cbeafe80d488850e
Andre Rothe <phosco@gmx.de>
2014-02-18 01:47:51 +0000

    ADD: a test case for issue 120
    git-svn-id: http://php-sql-parser.googlecode.com/svn/trunk@1081 44dcfd11-89b7-3d55-2063-5c365055f77f

---
+++ tests/cases/parser/issue120.php
@@ -0,0 +1,14 @@
+<?php
+require_once(dirname(__FILE__) . "/../../../src/PHPSQLParser.php");
+require_once(dirname(__FILE__) . "/../../test-more.php");
+
+$sql = "(
+SELECT CNAME
+FROM COCKTAIL
+)";
+$parser = new PHPSQLParser($sql, true);
+$p = $parser->parsed;
+$expected = getExpectedValue(dirname(__FILE__), 'issue120.serialized');
+eq_array($p, $expected, 'parentheses around select');
+
+?>
