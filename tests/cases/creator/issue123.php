44747ca40fed37ad01ac0048c25c9160856cf17a
Andre Rothe <phosco@gmx.de>
2014-02-21 08:43:36 +0000

    ADD: a test case for issue 123
    git-svn-id: http://php-sql-parser.googlecode.com/svn/trunk@1083 44dcfd11-89b7-3d55-2063-5c365055f77f

---
+++ tests/cases/creator/issue123.php
@@ -0,0 +1,12 @@
+<?php
+require_once dirname(__FILE__) . '/../../../src/PHPSQLParser.php';
+require_once dirname(__FILE__) . '/../../../src/PHPSQLCreator.php';
+require_once dirname(__FILE__) . '/../../test-more.php';
+
+$query = "CREATE TABLE t1 (c1 CHAR(3),c2 CHAR(3),KEY(c1))";
+$parser = new PHPSQLParser();
+$p = $parser->parse($query);
+$creator = new PHPSQLCreator();
+$created = $creator->create($p);
+$expected = getExpectedValue(dirname(__FILE__), 'issue123.sql', false);
+ok($created === $expected, 'create table with key column');
