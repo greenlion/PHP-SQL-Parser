6fa9499d27cb85c6f67ab982bc9dcde757c12213
Andre Rothe <phosco@gmx.de>
2014-02-25 20:36:31 +0000

    CHG: some code guideline requirements have been included.
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@1125 44dcfd11-89b7-3d55-2063-5c365055f77f

--- tests/cases/parser/inlist.php
+++ tests/cases/parser/inlist.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once(dirname(__FILE__) . "/../../../src/PHPSQLParser.php");
-require_once(dirname(__FILE__) . "/../../test-more.php");
+require_once dirname(__FILE__) . "/../../../src/PHPSQLParser.php";
+require_once dirname(__FILE__) . "/../../test-more.php";
 
 $parser = new PHPSQLParser();
 
@@ -9,6 +9,5 @@ $sql = "SELECT q.qid, question, gid FROM questions as q WHERE (select count(*) f
 $p = $parser->parse($sql);
 $expected = getExpectedValue(dirname(__FILE__), 'inlist1.serialized');
 eq_array($p, $expected, 'in list within WHERE clause');
-
 
 ?>
