9f9fd98e99f2230bf7778c8551c1cede0128bccf
Andre Rothe <phosco@gmx.de>
2014-02-25 10:45:14 +0000

    ADD: test cases for multi-table delete statements
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@1100 44dcfd11-89b7-3d55-2063-5c365055f77f

--- tests/cases/parser/delete.php
+++ tests/cases/parser/delete.php
@@ -1,13 +1,23 @@
 <?php
-require_once(dirname(__FILE__) . "/../../../src/PHPSQLParser.php");
-require_once(dirname(__FILE__) . "/../../test-more.php");
-
-$parser = new PHPSQLParser();
+require_once dirname(__FILE__) . "/../../../src/PHPSQLParser.php";
+require_once dirname(__FILE__) . "/../../test-more.php";
 
 $sql = "delete from testA as a where a.id = 1";
+$parser = new PHPSQLParser();
 $p = $parser->parse($sql);
 $expected = getExpectedValue(dirname(__FILE__), 'delete1.serialized');
 eq_array($p, $expected, 'simple delete statement');
 
+$sql = "DELETE t1, t2 FROM t1 INNER JOIN t2 INNER JOIN t3 WHERE t1.id=t2.id AND t2.id=t3.id";
+$parser = new PHPSQLParser();
+$p = $parser->parse($sql);
+$expected = getExpectedValue(dirname(__FILE__), 'delete2.serialized');
+eq_array($p, $expected, 'multi-table delete statement');
+
+$sql = "DELETE FROM t1.*, t2.* USING t1 INNER JOIN t2 INNER JOIN t3 WHERE t1.id=t2.id AND t2.id=t3.id;";
+$parser = new PHPSQLParser();
+$p = $parser->parse($sql);
+$expected = getExpectedValue(dirname(__FILE__), 'delete3.serialized');
+eq_array($p, $expected, 'multi-table delete statement with using');
 
 ?>
