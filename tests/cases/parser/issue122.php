8d2b4dc3ece3bcadc3e8e465943d927de6c83d30
Andre Rothe <phosco@gmx.de>
2014-02-25 09:08:00 +0000

    ADD: a test case for issue 122
    git-svn-id: http://php-sql-parser.googlecode.com/svn/trunk@1093 44dcfd11-89b7-3d55-2063-5c365055f77f

---
+++ tests/cases/parser/issue122.php
@@ -0,0 +1,11 @@
+<?php
+require_once(dirname(__FILE__) . "/../../../src/PHPSQLParser.php");
+require_once(dirname(__FILE__) . "/../../test-more.php");
+
+$sql = "UPDATE db.`tablename` SET a=1";
+$parser = new PHPSQLParser($sql, true);
+$p = $parser->parsed;
+$expected = getExpectedValue(dirname(__FILE__), 'issue122.serialized');
+eq_array($p, $expected, 'no_quotes property has been corrected');
+
+?>
