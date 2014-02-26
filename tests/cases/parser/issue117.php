317bbdfa2c8e76661ccd7a1c1967a43c8bd2020f
Andre Rothe <phosco@gmx.de>
2014-02-26 07:46:08 +0000

    CHG: add a TODO, because the ORDER BY clause has been lost.
    git-svn-id: http://php-sql-parser.googlecode.com/svn/trunk@1131 44dcfd11-89b7-3d55-2063-5c365055f77f

--- tests/cases/parser/issue117.php
+++ tests/cases/parser/issue117.php
@@ -1,7 +1,9 @@
 <?php
-require_once(dirname(__FILE__) . "/../../../src/PHPSQLParser.php");
-require_once(dirname(__FILE__) . "/../../test-more.php");
 
+require_once dirname(__FILE__) . "/../../../src/PHPSQLParser.php";
+require_once dirname(__FILE__) . "/../../test-more.php";
+
+// TODO: not solved, ORDER BY has been lost
 $sql = "(((SELECT x FROM table)) ORDER BY x)";
 $parser = new PHPSQLParser($sql, true);
 $p = $parser->parsed;
