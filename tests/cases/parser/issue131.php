0920e2822a8740ef773fe21d94f8f4f0a8666f23
Andre Rothe <phosco@gmx.de>
2014-03-04 11:22:25 +0000

    ADD: a test case for issue 131
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@1137 44dcfd11-89b7-3d55-2063-5c365055f77f

---
+++ tests/cases/parser/issue131.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once dirname(__FILE__) . "/../../../src/PHPSQLParser.php";
+require_once dirname(__FILE__) . "/../../test-more.php";
+
+$sql = "create unique index i1 using BTREE on t1 (c1(5) DESC, `col 2`(8) ASC) ALGORITHM=DEFAULT using hash LOCK=SHARED";
+$parser = new PHPSQLParser($sql);
+$p = $parser->parsed;
+$expected = getExpectedValue(dirname(__FILE__), 'issue131.serialized');
+eq_array($p, $expected, 'create index statement');
+
+?>
