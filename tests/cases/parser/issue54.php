3d6b3a854054f150afe8d6a2540d9e5b1e55c303
Andre Rothe <phosco@gmx.de>
2014-03-05 09:28:24 +0000

    ADD: a specific parser output test for HAVING clauses has been added.
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@1160 44dcfd11-89b7-3d55-2063-5c365055f77f

--- tests/cases/parser/issue54.php
+++ tests/cases/parser/issue54.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once dirname(__FILE__) . "/../../../src/PHPSQLParser.php";
 require_once dirname(__FILE__) . "/../../test-more.php";
 
@@ -11,11 +12,15 @@ function issue54ErrorHandler($errno, $errstr, $errfile, $errline) {
 }
 $old_error_handler = set_error_handler("issue54ErrorHandler");
 
-
 $parser = new PHPSQLParser();
 $sql = "SELECT schema.`table`.c as b, sum(id + 5 * (5 + 5)) as p FROM schema.table WHERE a=1 GROUP BY c HAVING p > 10 ORDER BY p DESC";
 $parser->parse($sql);
 $p = $parser->parsed;
+
 ok($errorNumber === 0, 'No notice should be thrown');
+$old_error_handler = set_error_handler($old_error_handler);
+
+$expected = getExpectedValue(dirname(__FILE__), 'issue54.serialized');
+eq_array($p, $expected, 'having clause and column references with schema/table/col parts.');
 
 ?>
