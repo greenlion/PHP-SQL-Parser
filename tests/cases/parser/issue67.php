b76355ff5812d7b5f880e49b3439d0f23adbd115
Andre Rothe <phosco@gmx.de>
2014-03-06 22:19:27 +0000

    CHG: the parser output has been enhnced by the SESSION/GLOBAL keyword.
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@1169 44dcfd11-89b7-3d55-2063-5c365055f77f

--- tests/cases/parser/issue67.php
+++ tests/cases/parser/issue67.php
@@ -3,14 +3,12 @@
 require_once dirname(__FILE__) . "/../../../src/PHPSQLParser.php";
 require_once dirname(__FILE__) . "/../../test-more.php";
 
-// TODO: the SET statement doesn't work completely, SESSION has been lost!
 $sql = "SET SESSION group_concat_max_len = @@max_allowed_packet";
 $parser = new PHPSQLParser($sql, true);
 $p = $parser->parsed;
 $expected = getExpectedValue(dirname(__FILE__), 'issue67a.serialized');
 eq_array($p, $expected, '@ character after operator should not fail.');
 
-// this is ok
 $sql = "SET @a = 1";
 $parser = new PHPSQLParser($sql, true);
 $p = $parser->parsed;
