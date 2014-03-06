1f2ac338a4116be47d8367ac527f7b86e7803e62
Andre Rothe <phosco@gmx.de>
2014-03-06 23:10:22 +0000

    CHG: some changes according to the code guidelines.
    git-svn-id: http://php-sql-parser.googlecode.com/svn/trunk@1170 44dcfd11-89b7-3d55-2063-5c365055f77f

--- tests/cases/parser/issue71.php
+++ tests/cases/parser/issue71.php
@@ -1,6 +1,7 @@
 <?php
-require_once(dirname(__FILE__) . "/../../../src/PHPSQLParser.php");
-require_once(dirname(__FILE__) . "/../../test-more.php");
+
+require_once dirname(__FILE__) . "/../../../src/PHPSQLParser.php";
+require_once dirname(__FILE__) . "/../../test-more.php";
 
 $sql = "select * from table1 as event";
 $parser = new PHPSQLParser($sql);
@@ -13,6 +14,5 @@ $parser = new PHPSQLParser($sql);
 $p = $parser->parsed;
 $expected = getExpectedValue(dirname(__FILE__), 'issue71b.serialized');
 eq_array($p, $expected, 'infinite loop on table alias "data"');
-
 
 ?>
