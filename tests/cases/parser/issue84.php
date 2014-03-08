12f78f88e3a3ad9360ab95da01f723af8cdfc6f4
Andre Rothe <phosco@gmx.de>
2014-03-08 01:35:58 +0000

    CHG: some changes according to the code guidelines.
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@1187 44dcfd11-89b7-3d55-2063-5c365055f77f

--- tests/cases/parser/issue84.php
+++ tests/cases/parser/issue84.php
@@ -1,6 +1,7 @@
 <?php
-require_once(dirname(__FILE__) . "/../../../src/PHPSQLParser.php");
-require_once(dirname(__FILE__) . "/../../test-more.php");
+
+require_once dirname(__FILE__) . "/../../../src/PHPSQLParser.php";
+require_once dirname(__FILE__) . "/../../test-more.php";
 
 $parser = new PHPSQLParser();
 $sql = "INSERT INTO newTablename SELECT field1, field2, field3 FROM oldTablename where field1 > 100";
@@ -24,4 +25,5 @@ $parser->parse($sql, true);
 $p = $parser->parsed;
 $expected = getExpectedValue(dirname(__FILE__), 'issue84c.serialized');
 eq_array($p, $expected, 'INSERT ... (cols) VALUES (values)');
+
 ?>
