87401c17e81de93bd12c1ace7366daaa5c6bdf37
Andre Rothe <phosco@gmx.de>
2014-03-08 14:15:57 +0000

    CHG: some changes according to the code guidelines
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@1200 44dcfd11-89b7-3d55-2063-5c365055f77f

--- tests/cases/parser/issue87.php
+++ tests/cases/parser/issue87.php
@@ -1,11 +1,8 @@
 <?php
-require_once(dirname(__FILE__) . "/../../../src/PHPSQLParser.php");
-require_once(dirname(__FILE__) . "/../../test-more.php");
 
-// TODO: we should store the object type under the RENAME field
-// because we can also rename users
-// after that we should use sub_tree to split all relevant keywords for the
-// position calculator
+require_once dirname(__FILE__) . "/../../../src/PHPSQLParser.php";
+require_once dirname(__FILE__) . "/../../test-more.php";
+
 $parser = new PHPSQLParser();
 $sql = "RENAME TABLE a TO b";
 $parser->parse($sql, true);
