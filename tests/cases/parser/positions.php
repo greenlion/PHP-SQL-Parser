9df607c8d309cf4f8749fda043251add08f074a3
Andre Rothe <phosco@gmx.de>
2014-03-09 16:12:03 +0000

    CHG: some changes according to the code guidelines.
    git-svn-id: http://php-sql-parser.googlecode.com/svn/trunk@1216 44dcfd11-89b7-3d55-2063-5c365055f77f

--- tests/cases/parser/positions.php
+++ tests/cases/parser/positions.php
@@ -1,6 +1,7 @@
 <?php
-require_once(dirname(__FILE__) . "/../../../src/PHPSQLParser.php");
-require_once(dirname(__FILE__) . "/../../test-more.php");
+
+require_once dirname(__FILE__) . "/../../../src/PHPSQLParser.php";
+require_once dirname(__FILE__) . "/../../test-more.php";
 
 $parser = new PHPSQLParser();
 
@@ -11,12 +12,14 @@ ok($p['SELECT'][0]['alias']['position'] == 12, 'position of column alias');
 ok($p['FROM'][0]['position'] == 23, 'position of table');
 ok($p['FROM'][0]['alias']['position'] == 28, 'position of table alias');
 
+
 $sql = "SELECT colA hello From test\nt";
 $p = $parser->parse($sql, true);
 ok($p['SELECT'][0]['position'] == 7, 'position of column');
 ok($p['SELECT'][0]['alias']['position'] == 12, 'position of column alias');
 ok($p['FROM'][0]['position'] == 23, 'position of table');
 ok($p['FROM'][0]['alias']['position'] == 28, 'position of table alias');
+
 
 $sql = "SELECT a.*, c.*, u.users_name FROM SURVEYS as a  INNER JOIN SURVEYS_LANGUAGESETTINGS as c ON ( surveyls_survey_id = a.sid AND surveyls_language = a.language ) AND surveyls_survey_id=a.sid and surveyls_language=a.language  INNER JOIN USERS as u ON (u.uid=a.owner_id)  ORDER BY surveyls_title";
 $p = $parser->parse($sql, true);
