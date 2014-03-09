7ba84bbbc6ff0b07edfb9082fcbc7fb0a3114069
Andre Rothe <phosco@gmx.de>
2014-03-09 22:44:31 +0000

    ADD: the test case for issue 104 has been added to the test suite.
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@1229 44dcfd11-89b7-3d55-2063-5c365055f77f

--- tests/tests-creator.php
+++ tests/tests-creator.php
@@ -29,6 +29,7 @@ require_once dirname(__FILE__) . '/cases/creator/issue98.php';
 require_once dirname(__FILE__) . '/cases/creator/issue100.php';
 require_once dirname(__FILE__) . '/cases/creator/issue101.php';
 require_once dirname(__FILE__) . '/cases/creator/issue102.php';
+require_once dirname(__FILE__) . '/cases/creator/issue104.php';
 require_once dirname(__FILE__) . '/cases/creator/issue105.php';
 require_once dirname(__FILE__) . '/cases/creator/issue106.php';
 require_once dirname(__FILE__) . '/cases/creator/issue110.php';
