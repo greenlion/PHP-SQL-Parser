4ac34d802830b957d77bb00371911d7634aab4e7
Andre Rothe <phosco@gmx.de>
2014-03-09 00:55:16 +0000

    CHG: the test case has been enhanced by some options of the INSERT statement.
    git-svn-id: http://php-sql-parser.googlecode.com/svn/trunk@1206 44dcfd11-89b7-3d55-2063-5c365055f77f

--- tests/cases/parser/issue90.php
+++ tests/cases/parser/issue90.php
@@ -1,10 +1,9 @@
 <?php
-require_once(dirname(__FILE__) . "/../../../src/PHPSQLParser.php");
-require_once(dirname(__FILE__) . "/../../test-more.php");
 
+require_once dirname(__FILE__) . "/../../../src/PHPSQLParser.php";
+require_once dirname(__FILE__) . "/../../test-more.php";
 
-// TODO: the keywords ON DUPLICATE KEY UPDATE are not parsed
-$sql = 'INSERT INTO table (a,b,c) VALUES (1,2,3) ON DUPLICATE KEY UPDATE id=LAST_INSERT_ID(id), c=3;';
+$sql = 'INSERT DELAYED IGNORE INTO table (a,b,c) VALUES (1,2,3) ON DUPLICATE KEY UPDATE id=LAST_INSERT_ID(id), c=3;';
 $parser = new PHPSQLParser();
 $p = $parser->parse($sql);
 $expected = getExpectedValue(dirname(__FILE__), 'issue90.serialized');
