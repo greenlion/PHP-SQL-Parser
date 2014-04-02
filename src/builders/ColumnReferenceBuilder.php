c891f131dbc99c12c5628c6cb4e0f95f6d2071f9
phosco@gmx.de <phosco@gmx.de@44dcfd11-89b7-3d55-2063-5c365055f77f>
2014-04-02 12:24:03 +0000

    CHG: The ColumnReferenceBuilder doesn't longer handle the direction keyword of an ORDER-BY clause. There is a sub-class, which should do that.
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@1263 44dcfd11-89b7-3d55-2063-5c365055f77f

--- src/builders/ColumnReferenceBuilder.php
+++ src/builders/ColumnReferenceBuilder.php
@@ -53,11 +53,6 @@ require_once dirname(__FILE__) . '/../utils/ExpressionType.php';
  */
 class ColumnReferenceBuilder implements Builder {
 
-    protected function buildDirection($parsed) {
-        $builder = new DirectionBuilder();
-        return $builder->build($parsed);
-    }
-
     protected function buildAlias($parsed) {
         $builder = new AliasBuilder();
         return $builder->build($parsed);
@@ -69,7 +64,6 @@ class ColumnReferenceBuilder implements Builder {
         }
         $sql = $parsed['base_expr'];
         $sql .= $this->buildAlias($parsed);
-        $sql .= $this->buildDirection($parsed);
         return $sql;
     }
 }
