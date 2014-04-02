2c0191dea2459a239845ffacc52814ae0fd2a36a
phosco@gmx.de <phosco@gmx.de@44dcfd11-89b7-3d55-2063-5c365055f77f>
2014-04-02 10:03:37 +0000

    CHG: the test case for issue 62 has been enhanced with cases for the modifications of marco64th.
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@1251 44dcfd11-89b7-3d55-2063-5c365055f77f

--- tests/cases/parser/issue62.php
+++ tests/cases/parser/issue62.php
@@ -6,7 +6,21 @@ require_once dirname(__FILE__) . "/../../test-more.php";
 $sql = "SELECT CAST((CONCAT(table1.col1,' ',time_start)) AS DATETIME) FROM table1";
 $parser = new PHPSQLParser($sql,true);
 $p = $parser->parsed;
-$expected = getExpectedValue(dirname(__FILE__), 'issue62.serialized');
+$expected = getExpectedValue(dirname(__FILE__), 'issue62a.serialized');
 eq_array($p, $expected, 'CAST expression');
+
+
+$sql = "UPDATE vtiger_tab set isentitytype=? WHERE tabid=?";
+$parser = new PHPSQLParser($sql,true);
+$p = $parser->parsed;
+$expected = getExpectedValue(dirname(__FILE__), 'issue62b.serialized');
+eq_array($p, $expected, '? after operand');
+
+
+$sql = "SELECT * FROM table1 IGNORE INDEX(PRIMARY)";
+$parser = new PHPSQLParser($sql,false);
+$p = $parser->parsed;
+$expected = getExpectedValue(dirname(__FILE__), 'issue62c.serialized');
+eq_array($p, $expected, 'IGNORE INDEX within FROM clause');
 
 ?>
