fdf8634e084e48540138e45c8d2d17d3c508356d
witchi <phosco@gmx.de>
2014-04-12 22:22:28 +0200

    CHG: the test case has been finished and it eturns "ok"

--- tests/cases/parser/customfunction.php
+++ tests/cases/parser/customfunction.php
@@ -7,13 +7,9 @@ try {
     $sql = "SELECT PERCENTILE(xyz, 90) as percentile from some_table";
     $parser = new PHPSQLParser();
     $parser->add_custom_function("percentile");
-    $p = $parser->parse($sql);
+    $p = $parser->parse($sql, true);
 } catch (Exception $e) {
     $p = array();
 }
-print_r($p);
-exit;
-ok($p['TABLE']['create-def']['sub_tree'][1]['sub_tree'][1]['sub_tree'][5]['expr_type'] === ExpressionType::DEF_VALUE,
-        'column definition with DEFAULT value');
-
+ok($p['SELECT'][0]['expr_type'] === ExpressionType::CUSTOM_FUNCTION, 'custom function within SELECT clause');
 ?>
