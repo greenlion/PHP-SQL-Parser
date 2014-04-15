6b411e76bba699febb2756d6bde58e90ca51adc2
phosco@gmx.de <phosco@gmx.de@44dcfd11-89b7-3d55-2063-5c365055f77f>
2014-04-15 12:15:46 +0000

    CHG: debug output has been removed.
    git-svn-id: https://php-sql-parser.googlecode.com/svn/trunk@1337 44dcfd11-89b7-3d55-2063-5c365055f77f

--- tests/cases/creator/issue126.php
+++ tests/cases/creator/issue126.php
@@ -46,7 +46,6 @@ require_once dirname(__FILE__) . '/../../test-more.php';
 $query = "DELETE FROM t1";
 $parser = new PHPSQLParser();
 $p = $parser->parse($query);
-print_r($p);
 $creator = new PHPSQLCreator();
 $created = $creator->create($p);
 $expected = getExpectedValue(dirname(__FILE__), 'issue126.sql', false);
