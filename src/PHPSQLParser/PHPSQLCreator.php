5ee666b5b7a65ee91f793a3d433644e741ffcd07
Steve Gallo <smgallo@buffalo.edu>
2016-01-14 16:13:07 -0500

    Revert "Improved handling of REPLACE"
    This reverts commit 48e87e4f9db533025cf24fb8c8bebab7d6788af6.

--- src/PHPSQLParser/PHPSQLCreator.php
+++ src/PHPSQLParser/PHPSQLCreator.php
@@ -45,7 +45,6 @@ use PHPSQLParser\builders\SelectStatementBuilder;
 use PHPSQLParser\builders\DeleteStatementBuilder;
 use PHPSQLParser\builders\UpdateStatementBuilder;
 use PHPSQLParser\builders\InsertStatementBuilder;
-use PHPSQLParser\builders\ReplaceStatementBuilder;
 use PHPSQLParser\builders\CreateStatementBuilder;
 use PHPSQLParser\builders\DropStatementBuilder;
 use PHPSQLParser\builders\RenameStatementBuilder;
@@ -81,10 +80,6 @@ class PHPSQLCreator {
             break;
         case 'INSERT':
             $builder = new InsertStatementBuilder();
-            $this->created = $builder->build($parsed);
-            break;
-        case 'REPLACE':
-            $builder = new ReplaceStatementBuilder();
             $this->created = $builder->build($parsed);
             break;
         case 'DELETE':
