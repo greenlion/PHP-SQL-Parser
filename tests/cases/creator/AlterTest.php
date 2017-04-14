9aabc758af8aa3abc8b3a35222de3abfb12faec4
vagrant <vagrant@postroom.(none)>
2017-04-14 23:42:34 +0000

    Uncommented test

--- tests/cases/creator/AlterTest.php
+++ tests/cases/creator/AlterTest.php
@@ -7,15 +7,15 @@ use PHPUnit_Framework_TestCase;
 
 class AlterTest extends PHPUnit_Framework_TestCase
 {
-//    public function testAlterChangeColumn()
-//    {
-//        $sql = "ALTER TABLE `user` CHANGE `id` `id` INT( 11 ) COMMENT 'id of user';";
-//        $parser = new PHPSQLParser($sql);
-//        $creator = new PHPSQLCreator($parser->parsed);
-//        $created = $creator->created;
-//        $expected = getExpectedValue(dirname(__FILE__), 'alter.sql', false);
-//        $this->assertSame($expected, $created, 'an alter table statement to change a column');
-//    }
+    public function testAlterChangeColumn()
+    {
+        $sql = "ALTER TABLE `user` CHANGE `id` `id` INT( 11 ) COMMENT 'id of user';";
+        $parser = new PHPSQLParser($sql);
+        $creator = new PHPSQLCreator($parser->parsed);
+        $created = $creator->created;
+        $expected = getExpectedValue(dirname(__FILE__), 'alter.sql', false);
+        $this->assertSame($expected, $created, 'an alter table statement to change a column');
+    }
 
     public function testAlterAddColumn()
     {
