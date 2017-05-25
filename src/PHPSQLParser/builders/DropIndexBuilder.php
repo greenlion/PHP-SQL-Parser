0f10466d299ba37c08a001f3d369b220dbf342ff
Iain <iain@polevaultweb.com>
2017-05-25 14:41:42 +0100

    Support parsing and creating DROP INDEX statements.

--- src/PHPSQLParser/builders/DropIndexBuilder.php
+++ src/PHPSQLParser/builders/DropIndexBuilder.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * DropStatement.php
+ * DropIndexBuilder.php
  *
- * Builds the DROP statement
+ * Builds the CREATE INDEX statement
  *
  * PHP version 5
  *
@@ -31,33 +31,26 @@
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  * @author    André Rothe <andre.rothe@phosco.info>
  * @copyright 2010-2014 Justin Swanhart and André Rothe
  * @license   http://www.debian.org/misc/bsd.license  BSD License (3 Clause)
  * @version   SVN: $Id$
- * 
+ *
  */
 
 namespace PHPSQLParser\builders;
 
 /**
- * This class implements the builder for the whole DROP TABLE statement. 
- * You can overwrite all functions to achieve another handling.
- *
- * @author  André Rothe <andre.rothe@phosco.info>
- * @license http://www.debian.org/misc/bsd.license  BSD License (3 Clause)
- *  
+ * This class implements the builder for the DROP INDEX statement. You can overwrite
+ * all functions to achieve another handling.
  */
-class DropStatementBuilder implements Builder {
+class DropIndexBuilder implements Builder {
 
-	protected function buildDROP( $parsed ) {
-		$builder = new DropBuilder();
-		return $builder->build( $parsed );
-	}
+    public function build(array $parsed) {
+        $sql = $parsed['name'];
+        return trim($sql);
+    }
 
-	public function build( array $parsed ) {
-		return $this->buildDROP( $parsed );
-	}
 }
 ?>
