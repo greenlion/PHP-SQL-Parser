89a52e10d06067bcc65eebe948ffbf8f8b385f45
Justin Swanhart <greenlion@gmail.com>
2020-10-01 08:49:27 -0400

    Revert "Update WhereBracketExpressionBuilder.php (#309)"
    This reverts commit ac9f3522185882712ffde63e679a5d037e06d84a.

--- src/PHPSQLParser/builders/WhereBracketExpressionBuilder.php
+++ src/PHPSQLParser/builders/WhereBracketExpressionBuilder.php
@@ -93,11 +93,6 @@ class WhereBracketExpressionBuilder implements Builder {
         return $builder->build($parsed);
     }
 
-    protected function buildSubQuery($parsed) {
-        $builder = new SubQueryBuilder();
-        return $builder->build($parsed);
-    }
-
     protected function buildReserved($parsed) {
       $builder = new ReservedBuilder();
       return $builder->build($parsed);
@@ -118,7 +113,6 @@ class WhereBracketExpressionBuilder implements Builder {
             $sql .= $this->buildWhereExpression($v);
             $sql .= $this->build($v);
             $sql .= $this->buildUserVariable($v);
-            $sql .= $this->buildSubQuery($v);
             $sql .= $this->buildReserved($v);
             $sql .= $this->buildSubQuery($v);
             
